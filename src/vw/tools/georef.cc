edda27901080245cb75facd563183627c622af8c
Matthew Hancher <matthew.d.hancher@nasa.gov>
2007-04-28 21:30:19 +0000

    Added new georef tool, make jp2dump file dependent on having a JPEG2K file driver.

---
+++ src/vw/tools/georef.cc
@@ -0,0 +1,218 @@
+// __BEGIN_LICENSE__
+// 
+// Copyright (C) 2006 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration
+// (NASA).  All Rights Reserved.
+// 
+// Copyright 2006 Carnegie Mellon University. All rights reserved.
+// 
+// This software is distributed under the NASA Open Source Agreement
+// (NOSA), version 1.3.  The NOSA has been approved by the Open Source
+// Initiative.  See the file COPYING at the top of the distribution
+// directory tree for the complete NOSA document.
+// 
+// THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
+// KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
+// LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
+// SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+// A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
+// THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
+// DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
+// 
+// __END_LICENSE__
+
+#ifdef _MSC_VER
+#pragma warning(disable:4244)
+#pragma warning(disable:4267)
+#pragma warning(disable:4996)
+#endif
+
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+#define VW_DEBUG_LEVEL 2
+
+#include <vw/Core/Cache.h>
+#include <vw/Core/ProgressCallback.h>
+#include <vw/Math/Matrix.h>
+#include <vw/Image/Transform.h>
+#include <vw/Image/Palette.h>
+#include <vw/FileIO/DiskImageResource.h>
+#include <vw/FileIO/DiskImageResourceGDAL.h>
+#include <vw/FileIO/DiskImageView.h>
+#include <vw/Cartography/GeoReference.h>
+#include <vw/Cartography/FileIO.h>
+using namespace vw;
+using namespace vw::math;
+using namespace vw::cartography;
+
+
+struct null_deleter {
+  void operator()(void const *) const {}
+};
+
+int main( int argc, char *argv[] ) {
+
+  std::string input_filename, output_filename, copy_filename;
+  double north_lat=90.0, south_lat=-90.0;
+  double east_lon=180.0, west_lon=-180.0;
+  double proj_lat=0, proj_lon=0, proj_scale=1;
+  unsigned utm_zone;
+  unsigned cache_size;
+  double nudge_x=0, nudge_y=0;
+
+  po::options_description general_options("General Options");
+  general_options.add_options()
+    ("output-file,o", po::value<std::string>(&output_filename)->default_value("output.tif"), "Specify the base output filename")
+    ("quiet,q", "Quiet output")
+    ("verbose,v", "Verbose output")
+    ("cache", po::value<unsigned>(&cache_size)->default_value(1024), "Cache size, in megabytes")
+    ("help", "Display this help message");
+
+  po::options_description projection_options("Projection Options");
+  projection_options.add_options()
+    ("copy", po::value<std::string>(&copy_filename), "Copy the projection from the given file")
+    ("north", po::value<double>(&north_lat), "The northernmost latitude in degrees")
+    ("south", po::value<double>(&south_lat), "The southernmost latitude in degrees")
+    ("east", po::value<double>(&east_lon), "The easternmost latitude in degrees")
+    ("west", po::value<double>(&west_lon), "The westernmost latitude in degrees")
+    ("sinusoidal", "Assume a sinusoidal projection")
+    ("mercator", "Assume a Mercator projection")
+    ("transverse-mercator", "Assume a transverse Mercator projection")
+    ("orthographic", "Assume an orthographic projection")
+    ("stereographic", "Assume a stereographic projection")
+    ("lambert-azimuthal", "Assume a Lambert azimuthal projection")
+    ("utm", po::value<unsigned>(&utm_zone), "Assume UTM projection with the given zone")
+    ("proj-lat", po::value<double>(&proj_lat), "The center of projection latitude (if applicable)")
+    ("proj-lon", po::value<double>(&proj_lon), "The center of projection longitude (if applicable)")
+    ("proj-scale", po::value<double>(&proj_scale), "The projection scale (if applicable)")
+    ("nudge-x", po::value<double>(&nudge_x), "Nudge the image, in projected coordinates")
+    ("nudge-y", po::value<double>(&nudge_y), "Nudge the image, in projected coordinates");
+  
+  po::options_description hidden_options("");
+  hidden_options.add_options()
+    ("input-file", po::value<std::string>(&input_filename));
+
+  po::options_description options("Allowed Options");
+  options.add(general_options).add(projection_options).add(hidden_options);
+
+  po::positional_options_description p;
+  p.add("input-file", -1);
+
+  po::variables_map vm;
+  po::store( po::command_line_parser( argc, argv ).options(options).positional(p).run(), vm );
+  po::notify( vm );
+
+  std::ostringstream usage;
+  usage << "Usage: image2kml [options] <filename>..." << std::endl << std::endl;
+  usage << general_options << std::endl;
+  usage << projection_options << std::endl;
+
+  if( vm.count("help") ) {
+    std::cout << usage.str();
+    return 1;
+  }
+
+  if( vm.count("input-file") < 1 ) {
+    std::cout << "Error: Must specify at least one input file!" << std::endl << std::endl;
+    std::cout << usage.str();
+    return 1;
+  }
+
+  if( vm.count("verbose") ) {
+    set_debug_level(VerboseDebugMessage);
+  }
+  else if( vm.count("quiet") ) {
+    set_debug_level(WarningMessage);
+  }
+
+  Cache::system_cache().resize( cache_size*1024*1024 );
+
+  GeoReference output_georef;
+  output_georef.set_well_known_geogcs("WGS84");
+  int total_resolution = 1024;
+
+  // Read in georeference info and compute total resolution
+  bool manual = vm.count("north") || vm.count("south") || vm.count("east") || vm.count("west");
+
+  DiskImageResourceGDAL file_resource( input_filename );
+  GeoReference georef;
+  if( vm.count("copy") ) {
+    DiskImageResourceGDAL copy_resource( copy_filename );
+    copy_resource.read_metadata( georef );
+  }
+  else {
+    file_resource.read_metadata( georef );
+  }
+
+  if ( georef.proj4_str() == "" ) georef.set_well_known_geogcs("WGS84");
+  if( manual || georef.transform() == identity_matrix<3>() ) {
+    vw_out(InfoMessage) << "No georeferencing info found.  Assuming Plate Carree WGS84: " 
+                        << east_lon << " to " << west_lon << " E, " << south_lat << " to " << north_lat << " N." << std::endl;
+    georef = GeoReference();
+    georef.set_well_known_geogcs("WGS84");
+    Matrix3x3 m;
+    m(0,0) = (east_lon - west_lon) / file_resource.cols();
+    m(0,2) = west_lon + 0.5*m(0,0);
+    m(1,1) = (south_lat - north_lat) / file_resource.rows();
+    m(1,2) = north_lat + 0.5*m(1,1);
+    m(2,2) = 1;
+    georef.set_transform( m );
+    manual = true;
+  }
+  else if( vm.count("sinusoidal") ) georef.set_sinusoidal(proj_lon);
+  else if( vm.count("mercator") ) georef.set_mercator(proj_lat,proj_lon,proj_scale);
+  else if( vm.count("transverse-mercator") ) georef.set_transverse_mercator(proj_lat,proj_lon,proj_scale);
+  else if( vm.count("orthographic") ) georef.set_orthographic(proj_lat,proj_lon);
+  else if( vm.count("stereographic") ) georef.set_stereographic(proj_lat,proj_lon,proj_scale);
+  else if( vm.count("lambert-azimuthal") ) georef.set_lambert_azimuthal(proj_lat,proj_lon);
+  else if( vm.count("utm") ) georef.set_UTM( utm_zone );
+
+  if( vm.count("nudge-x") || vm.count("nudge-y") ) {
+    Matrix3x3 m = georef.transform();
+    m(0,2) += nudge_x;
+    m(1,2) += nudge_y;
+    georef.set_transform( m );
+  }
+
+  // Our file readers do a better job that GDAL's of coping with large 
+  // image files in some cases, so we make a fresh DiskImageView rather 
+  // than making an ImageResourceView of the existing GDAL resource.
+
+  switch( file_resource.pixel_format() ) {
+  case VW_PIXEL_SCALAR: {
+    DiskImageView<uint8> input_image( input_filename );
+    write_georeferenced_image( output_filename, input_image, georef );
+    break;
+  }
+  case VW_PIXEL_GRAY: {
+    DiskImageView<PixelGray<uint8> > input_image( input_filename );
+    write_georeferenced_image( output_filename, input_image, georef );
+    break;
+  }
+  case VW_PIXEL_GRAYA: {
+    DiskImageView<PixelGrayA<uint8> > input_image( input_filename );
+    write_georeferenced_image( output_filename, input_image, georef );
+    break;
+  }
+  case VW_PIXEL_RGB: {
+    DiskImageView<PixelRGB<uint8> > input_image( input_filename );
+    write_georeferenced_image( output_filename, input_image, georef );
+    break;
+  }
+  case VW_PIXEL_RGBA: {
+    DiskImageView<PixelRGBA<uint8> > input_image( input_filename );
+    write_georeferenced_image( output_filename, input_image, georef );
+    break;
+  }
+  default: {
+    vw_throw( NoImplErr() << "Unsupported pixel format: " << file_resource.pixel_format() );
+    break;
+  }
+  }
+  return 0;
+}
