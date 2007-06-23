b674ba314e67b7814e07351a5efc7c922f5d374d
Michael Broxton <michael.broxton@nasa.gov>
2007-06-23 20:41:43 +0000

    Added a new image view: the DiskCacheImageView, which stores an image to a temporary file on disk, and presents the user with a DiskImageView-like interface to that cached image.  The temporary file is automatically deleted as soon as the DiskCacheImageView and any copies that are made fo it are destroyed.

--- src/vw/FileIO/DiskImageView.h
+++ src/vw/FileIO/DiskImageView.h
@@ -73,6 +73,84 @@ namespace vw {
 
   };
 
+
+  template <class PixelT>
+  class DiskCacheHandle {
+    DiskImageView<PixelT> m_disk_image_view;
+    std::string m_filename;
+
+  public:
+    template <class ViewT>
+    DiskCacheHandle(ImageViewBase<ViewT> const& view, std::string const& filename) :
+      m_disk_image_view(filename), m_filename(filename) {      
+    }
+    
+    ~DiskCacheHandle() { 
+      vw_out(DebugMessage) << "DiskCacheImageView: deleting temporary cache file: " << m_filename << "\n";
+      unlink(m_filename.c_str());
+    }
+
+    inline const DiskImageView<PixelT>& view() const { return m_disk_image_view; }
+  };
+
+  /// This is an assignable image view that stores the assigned data
+  /// to a temporary file on disk, and then provides a cached
+  /// interface (a la DiskImageView) to that data.  The temporary file
+  /// persists until this object and all copies of this object are
+  /// destroyed.
+  template <class PixelT>
+  class DiskCacheImageView : public ImageViewBase< DiskCacheImageView<PixelT> > {
+  private:                                      
+    boost::shared_ptr<DiskCacheHandle<PixelT> > m_handle;
+    std::string m_file_type;
+
+    template <class ViewT>
+    void initialize(ImageViewBase<ViewT> const& view) {
+      char base_name[] = "/tmp/vw_cache_XXXXXXX";
+      std::string filename = mktemp(base_name);
+      filename = filename + "." + m_file_type;
+      vw_out(InfoMessage) << "Creating disk cache of image in: " << filename << "\n";
+      write_image(filename, pixel_cast_rescale<PixelT>(view), TerminalProgressCallback());
+      m_handle = boost::shared_ptr<DiskCacheHandle<PixelT> >(new DiskCacheHandle<PixelT>(view.impl(), filename)); 
+    }
+
+  public:
+    typedef typename DiskImageView<PixelT>::pixel_type pixel_type;
+    typedef typename DiskImageView<PixelT>::result_type result_type;
+    typedef typename DiskImageView<PixelT>::pixel_accessor pixel_accessor;
+
+    /// Create a temporary image view cache file on disk using a
+    /// system supplied temporary filename.
+    template <class ViewT>
+    DiskCacheImageView(ImageViewBase<ViewT> const& view, std::string const& file_type = "tif") : m_file_type(file_type) {
+      this->initialize(view.impl());
+    }
+
+    inline int32 cols() const { return m_handle->view().cols(); }
+    inline int32 rows() const { return m_handle->view().rows(); }
+    inline int32 planes() const { return m_handle->view().planes(); }
+
+    inline pixel_accessor origin() const { return m_handle->view().origin(); }
+
+    inline result_type operator()( int32 i, int32 j, int32 p=0 ) const { return m_handle->view()(i, j, p); }
+
+    template <class ViewT>
+    DiskCacheImageView& operator=( ImageViewBase<ViewT> const& view ) {
+      this->initialize(view.impl());
+      return *this;
+    }
+
+    /// \cond INTERNAL
+    typedef typename DiskImageView<PixelT>::prerasterize_type prerasterize_type;
+    inline prerasterize_type prerasterize( BBox2i const& bbox ) const {
+      return m_handle->view().prerasterize(bbox);
+    }
+    template <class DestT> inline void rasterize( DestT const& dest, BBox2i const& bbox ) const {
+      vw::rasterize( prerasterize(bbox), dest, bbox );
+    }
+    /// \endcond
+  };
+
 } // namespace vw
 
 #endif // __VW_FILEIO_DISKIMAGEVIEW_H__
---
+++ src/vw/FileIO/tests/TestDiskImageView.h
@@ -0,0 +1,77 @@
+// __BEGIN_LICENSE__
+// 
+// Copyright (C) 2006 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration
+// (NASA).  All Rights Reserved.
+// 
+// Copyright 2006 Carnegie Mellon University. All rights reserved.
+// 
+// This software is distributed under the NASA Open Source Agreement
+// (NOSA), version 1.3.  The NOSA has been approved by the Open Source
+// Initiative.  See the file COPYING at the top of the distribution
+// directory tree for the complete NOSA document.
+// 
+// THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
+// KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
+// LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
+// SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+// A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
+// THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
+// DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
+// 
+// __END_LICENSE__
+
+// TestDiskImageResource.h
+#define CXXTEST_ABORT_TEST_ON_FAIL
+#include <cxxtest/TestSuite.h>
+#include <vw/FileIO/DiskImageView.h>
+
+#include <vw/config.h>
+#include <vw/Image/PixelTypes.h>
+#include <vw/Image/ImageView.h>
+#include <vw/Image/ImageMath.h>
+
+using namespace vw;
+
+class TestDiskImageView : public CxxTest::TestSuite
+{
+public:
+
+  void test_disk_cache_image_view() {
+#if defined(VW_HAVE_PKG_PNG) && VW_HAVE_PKG_PNG==1
+    ImageView<PixelRGB<uint8> > orig_image;
+    TS_ASSERT_THROWS_NOTHING( read_image( orig_image, "rgb2x2.png" ) );
+    
+    DiskCacheImageView<PixelRGB<uint8> > image = orig_image;
+
+    TS_ASSERT_EQUALS( image.cols(), 2 );
+    TS_ASSERT_EQUALS( image.rows(), 2 );
+    TS_ASSERT_EQUALS( image.planes(), 1 );
+    TS_ASSERT_EQUALS( image(0,0).r(), 128 );
+    TS_ASSERT_EQUALS( image(0,0).g(), 128 );
+    TS_ASSERT_EQUALS( image(0,0).b(), 128 );
+    TS_ASSERT_EQUALS( image(1,0).r(), 85 );
+    TS_ASSERT_EQUALS( image(1,0).g(), 0 );
+    TS_ASSERT_EQUALS( image(1,0).b(), 0 );
+    TS_ASSERT_EQUALS( image(0,1).r(), 0 );
+    TS_ASSERT_EQUALS( image(0,1).g(), 170 );
+    TS_ASSERT_EQUALS( image(0,1).b(), 0 );
+    TS_ASSERT_EQUALS( image(1,1).r(), 0 );
+    TS_ASSERT_EQUALS( image(1,1).g(), 0 );
+    TS_ASSERT_EQUALS( image(1,1).b(), 255 );
+
+    // Test copying
+    if (1) {
+      DiskCacheImageView<PixelRGB<uint8> > cache2 = image;
+      DiskCacheImageView<PixelRGB<uint8> > cache3 = cache2;
+    }
+
+    // Test re-assignment
+    image = orig_image + PixelRGB<uint8>(10,10,10);
+
+    // Test re-assignment after a copy
+    DiskCacheImageView<PixelRGB<uint8> > cache4 = image;
+    image = orig_image + PixelRGB<uint8>(20,20,20);
+#endif
+  }
+}; // class TestDiskImageResource
