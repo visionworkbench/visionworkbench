616ae3abc619bcad772061c28931ddde062d008d
Mike Lundy <mike.lundy@nasa.gov>
2008-08-31 22:53:00 +0000

    This is a hairy one. In the previous revision, there was a race between a call to Thread::id() and the static destruction of the Mutex. As far as I know, that could only happen if id() is called from a destructor. I suspect ~PerThreadBufferedStreamBuf -> sync() -> id(). id() used to win the race, but the upgrade to boost::thread 1.35 changed the order.
    Technically, this change causes a leak- ~Mutex is never called. For pthread's
    mutex, I don't think this matters, since it's leaked at program termination.
    For win32, I'm not sure.

--- src/vw/Core/Thread.cc
+++ src/vw/Core/Thread.cc
@@ -36,7 +36,16 @@ namespace thread {
   // Thread::id() method.
   static int vw_thread_next_available_id = 0;
   static boost::thread_specific_ptr<int> vw_thread_id_ptr;
-  static Mutex vw_thread_id_mutex; 
+
+  // The destructor for this Mutex is never called. This is intentional, to
+  // prevent static destruction. If Thread::id() is called from the destructor
+  // of a static object, there is a race between static destruction of the
+  // Mutex and the object whose destruction results in an id() call. The one I
+  // found was ~PerThreadBufferedStreamBuf -> sync() -> id()
+  static Mutex& vw_thread_id_mutex() {
+    static Mutex* m = new Mutex();
+    return *m;
+  }
 
 }} // namespace vw::thread
 
@@ -46,7 +55,7 @@ int vw::Thread::id() {
   // If the thread ID has not been accessed before, we initialize
   // it with a unique ID.
   if (thread::vw_thread_id_ptr.get() == 0) {
-    Mutex::Lock lock(thread::vw_thread_id_mutex);
+    Mutex::Lock lock(thread::vw_thread_id_mutex());
     thread::vw_thread_id_ptr.reset(new int(thread::vw_thread_next_available_id++));
   }
   
