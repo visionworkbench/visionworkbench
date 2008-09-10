2bc2c89110832dcea12636b9fc6570f62f1784fe
Mike Lundy <mike.lundy@nasa.gov>
2008-09-10 23:40:50 +0000

    clean up the blend tool a bit

--- src/vw/tools/blend.cc
+++ src/vw/tools/blend.cc
@@ -1,16 +1,16 @@
 // __BEGIN_LICENSE__
-// 
+//
 // Copyright (C) 2006 United States Government as represented by the
 // Administrator of the National Aeronautics and Space Administration
 // (NASA).  All Rights Reserved.
-// 
+//
 // Copyright 2006 Carnegie Mellon University. All rights reserved.
-// 
+//
 // This software is distributed under the NASA Open Source Agreement
 // (NOSA), version 1.3.  The NOSA has been approved by the Open Source
 // Initiative.  See the file COPYING at the top of the distribution
 // directory tree for the complete NOSA document.
-// 
+//
 // THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
 // KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
 // LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
@@ -18,7 +18,7 @@
 // A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
 // THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
 // DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
-// 
+//
 // __END_LICENSE__
 #ifdef _MSC_VER
 #pragma warning(disable:4244)
@@ -47,9 +47,11 @@ namespace fs = boost::filesystem;
 #include <vw/Mosaic/QuadTreeGenerator.h>
 #include <vw/Mosaic/ImageComposite.h>
 
-std::vector<std::string> image_files;
-std::string mosaic_name;
-std::string file_type;
+using namespace std;
+using namespace vw;
+
+string mosaic_name;
+string file_type;
 unsigned int cache_size;
 int patch_size, patch_overlap;
 bool draft;
@@ -57,15 +59,11 @@ bool qtree;
 
 template <class PixelT>
 void do_blend() {
-  vw::mosaic::ImageComposite<PixelT> composite;
+  mosaic::ImageComposite<PixelT> composite;
   if( draft ) composite.set_draft_mode( true );
 
-  for(unsigned int i=0; i < image_files.size(); i++) {
-      std::cout << "Adding file: " << image_files[i] << std::endl;
-  }
-
-  std::map<std::string,fs::path> image_files;
-  std::map<std::string,fs::path> offset_files;
+  map<string,fs::path> image_files;
+  map<string,fs::path> offset_files;
   fs::path source_dir_path( mosaic_name, fs::native );
   fs::directory_iterator pi( source_dir_path ), pend;
   for( ; pi != pend; ++pi ) {
@@ -73,34 +71,34 @@ void do_blend() {
       offset_files[basename(*pi)] = *pi;
     else image_files[basename(*pi)] = *pi;
   }
-  std::map<std::string,fs::path>::iterator ofi=offset_files.begin(), ofend=offset_files.end();
+  map<string,fs::path>::iterator ofi=offset_files.begin(), ofend=offset_files.end();
   for( ; ofi != ofend; ++ofi ) {
-    std::map<std::string,fs::path>::iterator ifi = image_files.find( ofi->first );
+    map<string,fs::path>::iterator ifi = image_files.find( ofi->first );
     if( ifi != image_files.end() ) {
       fs::ifstream offset( ofi->second );
       int x, y;
       offset >> x >> y;
-      std::cout << "Importing image file " << ifi->second.string() << " at offet (" << x << "," << y << ")" << std::endl;
-      composite.insert( vw::DiskImageView<PixelT>( ifi->second.string(), false ), x, y );
+      cout << "Importing image file " << ifi->second.string() << " at offet (" << x << "," << y << ")" << endl;
+      composite.insert( DiskImageView<PixelT>( ifi->second.string(), false ), x, y );
     }
   }
-    
-  vw::vw_out(vw::InfoMessage) << "Preparing the composite..." << std::endl;
+
+  vw_out(InfoMessage) << "Preparing the composite..." << endl;
   composite.prepare();
   if( qtree ) {
-    vw::vw_out(vw::InfoMessage) << "Preparing the quadtree..." << std::endl;
-    vw::mosaic::ImageQuadTreeGenerator<PixelT > quadtree( mosaic_name, composite );
+    vw_out(InfoMessage) << "Preparing the quadtree..." << endl;
+    mosaic::ImageQuadTreeGenerator<PixelT > quadtree( mosaic_name, composite );
     quadtree.set_output_image_file_type( file_type );
     quadtree.set_patch_size( patch_size );
     quadtree.set_patch_overlap( patch_overlap );
-    vw::vw_out(vw::InfoMessage) << "Generating..." << std::endl;
+    vw_out(InfoMessage) << "Generating..." << endl;
     quadtree.generate();
-    vw::vw_out(vw::InfoMessage) << "Done!" << std::endl;
+    vw_out(InfoMessage) << "Done!" << endl;
   }
   else {
-    vw::vw_out(vw::InfoMessage) << "Blending..." << std::endl;
+    vw_out(InfoMessage) << "Blending..." << endl;
     write_image( mosaic_name+".blend."+file_type, composite );
-    vw::vw_out(vw::InfoMessage) << "Done!" << std::endl;
+    vw_out(InfoMessage) << "Done!" << endl;
   }
 }
 
@@ -109,8 +107,8 @@ int main( int argc, char *argv[] ) {
     po::options_description desc("Options");
     desc.add_options()
       ("help", "Display this help message")
-      ("input-dir", po::value<std::string>(&mosaic_name), "Explicitly specify the input directory")
-      ("file-type", po::value<std::string>(&file_type)->default_value("png"), "Output file type")
+      ("input-dir", po::value<string>(&mosaic_name), "Explicitly specify the input directory")
+      ("file-type", po::value<string>(&file_type)->default_value("png"), "Output file type")
       ("patch-size", po::value<int>(&patch_size)->default_value(256), "Patch size, in pixels")
       ("patch-overlap", po::value<int>(&patch_overlap)->default_value(0), "Patch overlap, in pixels (must be even)")
       ("cache", po::value<unsigned>(&cache_size)->default_value(1024), "Cache size, in megabytes")
@@ -126,48 +124,48 @@ int main( int argc, char *argv[] ) {
     po::notify( vm );
 
     if( vm.count("help") ) {
-      std::cout << desc << std::endl;
+      cout << desc << endl;
       return 1;
     }
 
     if( vm.count("input-dir") != 1 ) {
-      std::cout << "Error: Must specify one (and only one) input directory!" << std::endl;
-      std::cout << desc << std::endl;
+      cout << "Error: Must specify one (and only one) input directory!" << endl;
+      cout << desc << endl;
       return 1;
     }
 
     if( vm.count("verbose") ) {
-      vw::set_debug_level(vw::VerboseDebugMessage);
+      set_debug_level(VerboseDebugMessage);
     }
 
     if( vm.count("draft") ) draft = true; else draft = false;
     if( vm.count("qtree") ) qtree = true; else qtree = false;
 
     if( patch_size <= 0 ) {
-      std::cerr << "Error: The patch size must be a positive number!  (You specified " << patch_size << ".)" << std::endl;
-      std::cout << desc << std::endl;
+      cerr << "Error: The patch size must be a positive number!  (You specified " << patch_size << ".)" << endl;
+      cout << desc << endl;
       return 1;
     }
 
     if( patch_overlap<0 || patch_overlap>=patch_size || patch_overlap%2==1 ) {
-      std::cerr << "Error: The patch overlap must be an even number nonnegative number" << std::endl;
-      std::cerr << "smaller than the patch size!  (You specified " << patch_overlap << ".)" << std::endl;
-      std::cout << desc << std::endl;
+      cerr << "Error: The patch overlap must be an even number nonnegative number" << endl;
+      cerr << "smaller than the patch size!  (You specified " << patch_overlap << ".)" << endl;
+      cout << desc << endl;
       return 1;
     }
 
-    vw::Cache::system_cache().resize( cache_size*1024*1024 );
+    Cache::system_cache().resize( cache_size*1024*1024 );
 
     if( vm.count("grayscale") ) {
-      do_blend<vw::PixelGrayA<float> >();
+      do_blend<PixelGrayA<float> >();
     }
     else {
-      do_blend<vw::PixelRGBA<float> >();
+      do_blend<PixelRGBA<float> >();
     }
 
   }
-  catch( std::exception &err ) {
-    vw::vw_out(vw::ErrorMessage) << "Error: " << err.what() << std::endl << "Aborting!" << std::endl;
+  catch( exception &err ) {
+    vw_out(ErrorMessage) << "Error: " << err.what() << endl << "Aborting!" << endl;
     return 1;
   }
   return 0;
