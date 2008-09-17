892e51ab88ed68478327530d2c81da80b367d145
Mike Lundy <mike.lundy@nasa.gov>
2008-09-17 02:35:20 +0000

    added a basic tester for foreach_ext

---
+++ src/vw/tests/TestExtTester.h
@@ -0,0 +1,47 @@
+// __BEGIN_LICENSE__
+//
+// Copyright (C) 2006 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration
+// (NASA).  All Rights Reserved.
+//
+// Copyright 2006 Carnegie Mellon University. All rights reserved.
+//
+// This software is distributed under the NASA Open Source Agreement
+// (NOSA), version 1.3.  The NOSA has been approved by the Open Source
+// Initiative.  See the file COPYING at the top of the distribution
+// directory tree for the complete NOSA document.
+//
+// THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
+// KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
+// LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
+// SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+// A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
+// THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
+// DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
+//
+// __END_LICENSE__
+
+// TestBlockFileIO.h
+#define CXXTEST_ABORT_TEST_ON_FAIL
+#include <cxxtest/TestSuite.h>
+#include <string>
+#include <vw/Core/Log.h>
+#include <vw/FileIO/DiskImageResource_internal.h>
+
+using namespace vw;
+using namespace vw::internal;
+
+static void test_extension(std::string fn)
+{
+  TS_TRACE(vw::stringify("Testing ") + fn);
+}
+
+class TestExtTester : public CxxTest::TestSuite
+{
+public:
+
+  void test_tester()
+  {
+    foreach_ext("rwtest", test_extension);
+  }
+}; // class TestBlockFileIO
