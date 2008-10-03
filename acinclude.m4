6211110347c1aa479193dddeeae45fb6118d501a
Mike Lundy <mike.lundy@nasa.gov>
2008-10-03 00:25:56 +0000

    This adds support for replacing the c++ terminate handler. It could be considered a work in progress. I /think/ I've covered detecting and disabling it where necessary, but please give it some testing. To use it, you can do something like this:
    TerminateHandler th[] = {BacktraceTerminate, PrettyTerminate, DefaultTerminate};
    
    for (int i = 0; i < 3; ++i)
      if (set_terminate_handler(th[i]))
        break;
    
    As it is, this really only supports g++, and should be automatically
    disabled if you try it with others. It is possible to write an
    implementation of this for vc++, though, so if someone wants to give it a
    shot...

--- acinclude.m4
+++ acinclude.m4
@@ -891,3 +891,13 @@ AC_DEFUN([AX_MODULE],
   #  backwards compatability with older versions of automake.
   #  AM_CONDITIONAL([MAKE_MODULE_$1], [test "$MAKE_MODULE_$1" = "yes"])
 ])
+
+# This checks for various introspection functions
+AC_DEFUN([AX_CHECK_INTROSPECTION],
+[
+    AC_CHECK_HEADERS(execinfo.h cxxabi.h typeinfo dlfcn.h)
+
+    AC_CHECK_FUNCS([__cxa_current_exception_type __cxa_demangle backtrace])
+
+    AC_SEARCH_LIBS(dladdr, dl, [AC_DEFINE(HAVE_DLADDR,1,Define if you have dladdr())])
+])
---
+++ src/vw/Core/TerminationHandler.h
@@ -0,0 +1,26 @@
+#ifndef __VW_CORE_TERMINATION_HANDLER_H__
+#define __VW_CORE_TERMINATION_HANDLER_H__
+
+#include <boost/function.hpp>
+
+namespace vw {
+
+  // Handlers for terminate().
+  typedef boost::function<void()> TerminateHandler;
+
+  // Some built-in handlers to choose from.
+  extern const TerminateHandler DefaultTerminate;
+  extern const TerminateHandler PrettyTerminate;
+  extern const TerminateHandler BacktraceTerminate;
+
+  // Sets the application-wide terminate() handler. Pass zero to reinstate
+  // the default. If you want the program to hang so you can attach gdb, your
+  // TerminateHandler should not call abort!  The return value will tell you
+  // whether the new handler was actually installed. This is useful if
+  // PrettyTerminate or BacktraceTerminate are not supported in your build.
+  bool set_terminate_handler( TerminateHandler const &th );
+
+} // namespace vw
+
+
+#endif
