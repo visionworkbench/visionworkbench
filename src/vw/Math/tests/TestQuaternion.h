91f6e15a4f5688114915fe0a620c777f40146b93
Matthew Hancher <matthew.d.hancher@nasa.gov>
2009-01-01 19:56:16 +0000

    Incorporate more matrix/quaternion conversion tests.

--- src/vw/Math/tests/TestQuaternion.h
+++ src/vw/Math/tests/TestQuaternion.h
@@ -113,4 +113,79 @@ public:
     TS_ASSERT_DELTA(sol1(2),sol4(2),1e-9);
   }
 
+
+  void test_rotationmatrix() {
+    vw::Matrix3x3 I;
+    I.set_identity();
+
+    vw::Matrix3x3 T, dQ;
+
+
+    // test ww
+    double R_data_ww[9] = { 0.36, 0.48, -0.8, -0.8, 0.6, 0, 0.48, 0.64, 0.6 };
+    vw::Matrix3x3 R_ww(R_data_ww);
+
+    // check if R*transpose(R) is identity
+    T = R_ww * transpose(R_ww) - I;
+    for (Matrix3x3::const_iterator it = T.begin(); it != T.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+    // converting the matrix to quaternion and back should not change it
+    dQ = R_ww - vw::Quaternion<double>(R_ww).rotation_matrix();
+    for (Matrix3x3::const_iterator it = dQ.begin(); it != dQ.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+
+    // test xx
+    double R_data_xx[9] = { 0.11276615807984,   -0.0161365698014631,  0.993490515660293,
+			    -0.633815522454304, -0.771198883306014,   0.0594151990953122,
+			    0.765220018744864, -0.636389733970151,  -0.097192743605217,
+    };
+    vw::Matrix3x3 R_xx(R_data_xx);
+
+    // check if is identity
+    T = R_xx * transpose(R_xx) - I;
+    for (Matrix3x3::const_iterator it = T.begin(); it != T.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+    // converting the matrix to quaternion and back should not change it
+    dQ = R_xx - vw::Quaternion<double>(R_xx).rotation_matrix();
+    for (Matrix3x3::const_iterator it = dQ.begin(); it != dQ.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+
+    // test yy
+    double R_data_yy[9] = { -0.771198883306014, -0.633815522454304,  0.0594151990953122,
+			    -0.0161365698014631, 0.11276615807984,   0.993490515660293,
+			    -0.636389733970151,  0.765220018744864, -0.097192743605217,
+    };
+    vw::Matrix3x3 R_yy(R_data_yy);
+
+    // check if is identity
+    T = R_yy * transpose(R_yy) - I;
+    for (Matrix3x3::const_iterator it = T.begin(); it != T.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+    // converting the matrix to quaternion and back should not change it
+    dQ = R_yy - vw::Quaternion<double>(R_yy).rotation_matrix();
+    for (Matrix3x3::const_iterator it = dQ.begin(); it != dQ.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+
+    // test zz
+    double R_data_zz[9] = { -0.771198883306014,  0.0594151990953122, -0.633815522454304,
+			    -0.636389733970151, -0.097192743605217,   0.765220018744864,
+			    -0.0161365698014631, 0.993490515660293,   0.11276615807984 
+    };
+    vw::Matrix3x3 R_zz(R_data_zz);
+
+    T = R_zz * transpose(R_zz) - I;
+    for (Matrix3x3::const_iterator it = T.begin(); it != T.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+
+    // converting the matrix to quaternion and back should not change it
+    dQ = R_zz - vw::Quaternion<double>(R_zz).rotation_matrix();
+    for (Matrix3x3::const_iterator it = dQ.begin(); it != dQ.end(); ++it)
+      TS_ASSERT_DELTA(*it, 0, 1e-14);
+  }
 }; // class TestQuaternion
