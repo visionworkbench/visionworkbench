54b362b51775baf3aa50b2f84c6b7f1921099a36
Mike Lundy <mike.lundy@nasa.gov>
2009-01-24 01:48:47 +0000

    forgot to check these in.

---
+++ src/vw/Geometry/tests/TestInstantiateRecordList.hh
@@ -0,0 +1,5 @@
+template class  vw::geometry::Box<RealT, DimN>;
+template class  vw::geometry::ShapeBase<ShapeT, RealT, DimN>;
+template class  vw::geometry::Sphere<RealT, 0>;
+template class  vw::geometry::Sphere<RealT, DimN>;
+template class  vw::geometry::SphereBase<SphereT, RealT, DimN>;
