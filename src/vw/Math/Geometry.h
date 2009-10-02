e1f37f3865e7268429a64267561c1e04f145f153
Kyle Husmann <kyle.husmann@gmail.com>
2009-10-02 23:52:37 +0000

    Fixed indents, converted tabs->spaces

--- src/vw/Math/Geometry.h
+++ src/vw/Math/Geometry.h
@@ -32,16 +32,14 @@ namespace math {
 
     /// A homography requires at least 4 point matches to determine 8 unknowns
     template <class ContainerT>
-    unsigned min_elements_needed_for_fit(ContainerT const& example) const {
-      return 4;
-    }
+    unsigned min_elements_needed_for_fit(ContainerT const& example) const { return 4; }
 
     // Applies a transform matrix to a list of points;
     std::vector<Vector<double> > apply_matrix( Matrix<double> const& m,
-					       std::vector<Vector<double> > const& pts ) const {
+                 std::vector<Vector<double> > const& pts ) const {
       std::vector<Vector<double> > out;
       for ( unsigned i = 0; i < pts.size(); i++ ) {
-	out.push_back( m*pts[i] );
+        out.push_back( m*pts[i] );
       }
       return out;
     }
@@ -58,10 +56,10 @@ namespace math {
       Vector<double> sum;
       sum.set_size(dimension-1);
       for ( unsigned i = 0; i < num_points; i++ )
-	sum+=subvector(pts[i],0,dimension-1);
+        sum+=subvector(pts[i],0,dimension-1);
       sum /= num_points;
       for ( unsigned i = 0; i < dimension-1; i++ )
-	translation(i,dimension-1) = -sum(i);
+        translation(i,dimension-1) = -sum(i);
 
       std::vector<Vector<double> > pts_int = apply_matrix( translation, pts );
 
@@ -69,7 +67,7 @@ namespace math {
       scalar.set_identity(dimension);
       double scale = 0;
       for ( unsigned i = 0; i < num_points; i++ )
-	scale += norm_2( subvector(pts_int[i],0,dimension-1) );
+        scale += norm_2( subvector(pts_int[i],0,dimension-1) );
       scale = num_points*sqrt(2)/scale;
       scalar *= scale;
       scalar(dimension-1,dimension-1) = 1;
@@ -77,35 +75,35 @@ namespace math {
     }
 
     vw::Matrix<double> BasicDLT( std::vector<Vector<double> > const& input,
-				 std::vector<Vector<double> > const& output )  const {
+                                 std::vector<Vector<double> > const& output )  const {
       VW_ASSERT( input.size() == 4 && output.size() == 4,
-		 vw::ArgumentErr() << "DLT in this implementation expects to have only 4 inputs." );
-      VW_ASSERT( input[0][input[0].size()-1] == 1,
-		 vw::ArgumentErr() << "Input data doesn't seem to be normalized.");
+                 vw::ArgumentErr() << "DLT in this implementation expects to have only 4 inputs." );
+      VW_ASSERT( input[0][input[0].size()-1] == 1, 
+                 vw::ArgumentErr() << "Input data doesn't seem to be normalized.");
       VW_ASSERT( output[0][output[0].size()-1] == 1,
-		 vw::ArgumentErr() << "Secondary input data doesn't seem to be normalized.");
+                 vw::ArgumentErr() << "Secondary input data doesn't seem to be normalized.");
       VW_ASSERT( input[0].size() == 3,
-		 vw::ArgumentErr() << "Unfortunately at this time, BasicDLT only support homogeneous 2D vectors.");
+                 vw::ArgumentErr() << "Unfortunately at this time, BasicDLT only support homogeneous 2D vectors.");
       
       vw::Matrix<double,8,9> A;
       for ( unsigned i = 0; i < 4; i++ )
-	for ( unsigned j = 0; j < 3; j++ ) {
-	  // Filling in -wi'*xi^T
-	  A(i,j+3) = -output[i][2]*input[i][j];
-	  // Filling in yi'*xi^T
-	  A(i,j+6) = output[i][1]*input[i][j];
-	  // Filling in wi'*xi^T
-	  A(i+4,j) = output[i][2]*input[i][j];
-	  // Filling in -xi'*xi^T
-	  A(i+4,j+6) = -output[i][0]*input[i][j];
-	}
+        for ( unsigned j = 0; j < 3; j++ ) {
+          // Filling in -wi'*xi^T
+          A(i,j+3) = -output[i][2]*input[i][j];
+          // Filling in yi'*xi^T
+          A(i,j+6) = output[i][1]*input[i][j];
+          // Filling in wi'*xi^T
+          A(i+4,j) = output[i][2]*input[i][j];
+          // Filling in -xi'*xi^T
+          A(i+4,j+6) = -output[i][0]*input[i][j];
+        }
 
       Matrix<double> nullspace = null(A);
       nullspace /= nullspace(8,0);
       Matrix<double,3,3> H;
       for ( unsigned i = 0; i < 3; i++ )
-	for ( unsigned j = 0; j < 3; j++ )
-	  H(i,j) = nullspace(i*3+j,0);
+        for ( unsigned j = 0; j < 3; j++ )
+          H(i,j) = nullspace(i*3+j,0);
       return H;
     }
 
@@ -133,25 +131,24 @@ namespace math {
 
       // Evaluator
       inline result_type operator()( domain_type const& x ) const {
-	result_type output;
-	output.set_size(m_measure.size());
-	Matrix3x3 H;
-	for ( unsigned i = 0; i < 3; i++ )
-	  for ( unsigned j = 0; j < 3; j++ )
-	    if ( i != 2 || j != 2 )
-	      H(i,j) = x( 3*i+j );
-	    else
-	      H(2,2) = 1;
+        result_type output;
+        output.set_size(m_measure.size());
+        Matrix3x3 H;
+        for ( unsigned i = 0; i < 3; i++ )
+          for ( unsigned j = 0; j < 3; j++ )
+            if ( i != 2 || j != 2 )
+              H(i,j) = x( 3*i+j );
+            else
+              H(2,2) = 1;
 
-	for ( unsigned i = 0; i < m_measure.size(); i+=2 ) {
-	  Vector3 input( m_measure[i],
-			 m_measure[i+1], 1 );
-	  Vector3 output_i = H*input;
-	  output_i /= output_i(2);
-	  output(i) = output_i(0);
-	  output(i+1) = output_i(1);
-	}
-	return output;
+        for ( unsigned i = 0; i < m_measure.size(); i+=2 ) {
+          Vector3 input( m_measure[i], m_measure[i+1], 1 );
+          Vector3 output_i = H*input;
+          output_i /= output_i(2);
+          output(i) = output_i(0);
+          output(i+1) = output_i(1);
+        }
+        return output;
       }
     };
 
@@ -160,90 +157,86 @@ namespace math {
     /// a minimization algorithm.
     template <class ContainerT>
     vw::Matrix<double> operator()( std::vector<ContainerT> const& p1,
-				   std::vector<ContainerT> const& p2,
-				   vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
+           std::vector<ContainerT> const& p2,
+           vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
       VW_ASSERT( p1.size() == p2.size(),
-		 vw::ArgumentErr() << "Cannot compute homography. p1 and p2 are not the same size." );
+                 vw::ArgumentErr() << "Cannot compute homography. p1 and p2 are not the same size." );
       VW_ASSERT( p1.size() >= min_elements_needed_for_fit(p1[0]),
-		 vw::ArgumentErr() << "Cannot compute homography. Insufficient data.");
+                 vw::ArgumentErr() << "Cannot compute homography. Insufficient data.");
       VW_ASSERT( p1[0].size() == 3,
-		 vw::ArgumentErr() << "Cannot compute homography. Currently only support homogeneous 2D vectors." );
+                 vw::ArgumentErr() << "Cannot compute homography. Currently only support homogeneous 2D vectors." );
       VW_ASSERT( p1[0][2] == 1,
-		 vw::ArgumentErr() << "Cannot compute homography. Vectors have not been normalized.");
+                 vw::ArgumentErr() << "Cannot compute homography. Vectors have not been normalized.");
       
       // Converting to a container that is used internally.
       std::vector<Vector<double> > input;
       std::vector<Vector<double> > output;
       for ( unsigned i = 0; i < p1.size(); i++ )
-	input.push_back( Vector3( p1[i][0], p1[i][1], 1 ) );
+        input.push_back( Vector3( p1[i][0], p1[i][1], 1 ) );
       for ( unsigned i = 0; i < p2.size(); i++ ) 
-	output.push_back( Vector3( p2[i][0], p2[i][1], 1 ) );
+        output.push_back( Vector3( p2[i][0], p2[i][1], 1 ) );
 
       unsigned num_points = p1.size();
       if ( num_points == min_elements_needed_for_fit(p1[0] ) ) {
-	// Use DLT
-	Matrix<double> S_in = NormSimilarity(input);
-	Matrix<double> S_out = NormSimilarity(output);
-	std::vector<Vector<double> > input_prime = apply_matrix( S_in,
-								 input );
-	std::vector<Vector<double> > output_prime = apply_matrix( S_out,
-								  output );
-	Matrix<double> H_prime = BasicDLT( input_prime,
-					   output_prime );
-	Matrix<double> H = inverse(S_out)*H_prime*S_in;
-	H /= H(2,2);
-	return H;
+        // Use DLT
+        Matrix<double> S_in = NormSimilarity(input);
+        Matrix<double> S_out = NormSimilarity(output);
+        std::vector<Vector<double> > input_prime = apply_matrix( S_in, input );
+        std::vector<Vector<double> > output_prime = apply_matrix( S_out, output );
+        Matrix<double> H_prime = BasicDLT( input_prime, output_prime );
+        Matrix<double> H = inverse(S_out)*H_prime*S_in;
+        H /= H(2,2);
+        return H;
       } else {
-	// Levenberg Marquardt method for solving for homography.
-	// - The error metric is x2 - norm(H*x1). 
-	// - measure in x1 are fixed.
-	// - Unfortunately at this time. Error in x1 are not dealt
-	// with and I need more time to read up on other error
-	// metrics.
+        // Levenberg Marquardt method for solving for homography.
+        // - The error metric is x2 - norm(H*x1). 
+        // - measure in x1 are fixed.
+        // - Unfortunately at this time. Error in x1 are not dealt
+        // with and I need more time to read up on other error
+        // metrics.
 
-	// Copying seed
-	Matrix<double> seed_copy = seed_input;
-	if ( seed_copy.cols() != 3 ||
-	     seed_copy.rows() != 3 ) {
-	  std::vector<ContainerT> p1_small, p2_small;
-	  for ( unsigned i = 0; i < 4; i++ ) {
-	    p1_small.push_back( p1[i] );
-	    p2_small.push_back( p2[i] );
-	  }
-	  seed_copy = HomographyFittingFunctor()(p1_small, p2_small);
-	}
+        // Copying seed
+        Matrix<double> seed_copy = seed_input;
+        if ( seed_copy.cols() != 3 || seed_copy.rows() != 3 ) {
+          std::vector<ContainerT> p1_small, p2_small;
+          for ( unsigned i = 0; i < 4; i++ ) {
+            p1_small.push_back( p1[i] );
+            p2_small.push_back( p2[i] );
+          }
+          seed_copy = HomographyFittingFunctor()(p1_small, p2_small);
+        }
 
-	// Flatting input & output;
-	Vector<double> input_flat( input.size()*2 );
-	Vector<double> output_flat( output.size()*2 );
-	for ( unsigned i = 0; i < input.size(); i++ ) {
-	  subvector(input_flat,i*2,2) = subvector(input[i],0,2);
-	  subvector(output_flat,i*2,2) = subvector(output[i],0,2);
-	}
+        // Flatting input & output;
+        Vector<double> input_flat( input.size()*2 );
+        Vector<double> output_flat( output.size()*2 );
+        for ( unsigned i = 0; i < input.size(); i++ ) {
+          subvector(input_flat,i*2,2) = subvector(input[i],0,2);
+          subvector(output_flat,i*2,2) = subvector(output[i],0,2);
+        }
 
-	HomographyModelLMA model( input_flat );
+        HomographyModelLMA model( input_flat );
 
-	// Flatting Homography matrix into 8-vector
-	Vector<double> seed(8);
-	for ( unsigned i = 0; i < 3; i++ )
-	  for ( unsigned j = 0; j < 3; j++ )
-	    if ( i != 2 || j != 2 )
-	      seed( i*3+j ) = seed_copy(i,j);
+        // Flatting Homography matrix into 8-vector
+        Vector<double> seed(8);
+        for ( unsigned i = 0; i < 3; i++ )
+          for ( unsigned j = 0; j < 3; j++ )
+            if ( i != 2 || j != 2 )
+              seed( i*3+j ) = seed_copy(i,j);
 
-	int status = 0;
-	Vector<double> result_flat = levenberg_marquardt( model, seed,
-							  output_flat, status );
+        int status = 0;
+        Vector<double> result_flat = levenberg_marquardt( model, seed,
+                                                          output_flat, status );
+        
+        // Unflatting result
+        Matrix3x3 result;
+        for ( unsigned i = 0; i < 3; i++ )
+          for ( unsigned j = 0; j < 3; j++ )
+            if ( i != 2 || j != 2 )
+              result(i,j) = result_flat( 3*i+j );
+            else
+              result(2,2) = 1;
 
-	// Unflatting result
-	Matrix3x3 result;
-	for ( unsigned i = 0; i < 3; i++ )
-	  for ( unsigned j = 0; j < 3; j++ )
-	    if ( i != 2 || j != 2 )
-	      result(i,j) = result_flat( 3*i+j );
-	    else
-	      result(2,2) = 1;
-
-	return result;
+        return result;
       }
     }
   };
@@ -268,7 +261,7 @@ namespace math {
     template <class ContainerT>
     vw::Matrix<double> operator() (std::vector<ContainerT> const& p1, 
                                    std::vector<ContainerT> const& p2,
-				   vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
+                                   vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
 
       // check consistency
       VW_ASSERT( p1.size() == p2.size(), 
@@ -332,7 +325,7 @@ namespace math {
     template <class ContainerT>
     vw::Matrix<double> operator() (std::vector<ContainerT> const& p1, 
                                    std::vector<ContainerT> const& p2,
-				   vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
+           vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
 
       // check consistency
       VW_ASSERT( p1.size() == p2.size(), 
@@ -388,11 +381,11 @@ namespace math {
     }
   };
 
-  /// This fitting functor attempts to find an translation rotation transformation
+  /// This fitting functor attempts to find a translation and rotation transformation
   struct TranslationRotationFittingFunctor {
     typedef vw::Matrix<double,3,3> result_type;
 
-    /// A translation rotation requires 2 pairs of data points to make a fit.
+    /// A transformation requires 2 pairs of data points to make a fit.
     template <class ContainerT>
     unsigned min_elements_needed_for_fit(ContainerT const& example) const { return 2; }
 
@@ -403,7 +396,7 @@ namespace math {
     template <class ContainerT>
     vw::Matrix<double> operator() (std::vector<ContainerT> const& p1, 
                                    std::vector<ContainerT> const& p2,
-				   vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
+           vw::Matrix<double> const& seed_input = vw::Matrix<double>() ) const {
 
       // check consistency
       VW_ASSERT( p1.size() == p2.size(), 
