c97a7a2944ad19de84a80b830fcf67e63ef603da
Kyle Husmann <kyle.husmann@gmail.com>
2009-10-22 05:50:10 +0000

    removed broken code to avoid processing blank space. also, this code wasn't even necessary because the subregioning stuff avoids processing blank space anyway

--- src/vw/Stereo/PyramidCorrelator.h
+++ src/vw/Stereo/PyramidCorrelator.h
@@ -101,9 +101,6 @@ namespace stereo {
     std::vector<BBox2i> subdivide_bboxes(ImageView<PixelMask<Vector2f> > const& disparity_map,
                                          BBox2i const& box);
 
-    bool has_valid_mask_overlap( ImageView<vw::uint8> const& left_mask,
-                                 ImageView<vw::uint8> const& right_mask );
-
     // do_correlation()
     //
     // Takes an image pyramid of images and conducts dense stereo
@@ -138,8 +135,7 @@ namespace stereo {
         std::vector<BBox2> search_ranges;
         std::vector<BBox2i> nominal_blocks;
         if (n == (m_pyramid_levels-1) ) {
-          if ( has_valid_mask_overlap(left_masks[n], right_masks[n]) )
-            nominal_blocks.push_back(BBox2i(0,0,left_pyramid[n].cols(), left_pyramid[n].rows()));
+          nominal_blocks.push_back(BBox2i(0,0,left_pyramid[n].cols(), left_pyramid[n].rows()));
           search_ranges.push_back(initial_search_range);
         } else {
           //nominal_blocks = image_blocks(left_pyramid[n], 512, 512);
