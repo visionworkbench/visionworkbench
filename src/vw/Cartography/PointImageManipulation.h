76087d043a42e3330180fddc70f3550b3b587a19
Kyle Husmann <kyle.husmann@gmail.com>
2009-11-28 00:57:53 +0000

    added a function to convert dems to point images

--- src/vw/Cartography/PointImageManipulation.h
+++ src/vw/Cartography/PointImageManipulation.h
@@ -10,6 +10,7 @@
 
 #include <vw/Math/Vector.h>
 #include <vw/Image/PerPixelViews.h>
+#include <vw/Image/UtilityViews.h>
 #include <vw/Cartography/GeoReference.h>
 
 // This include is here to keep compat (the contents of that header used to be
@@ -44,22 +45,43 @@ namespace cartography {
   };
 
   // This version of the functor assumes that the point inputs are
-  // unprojected (lon, lat, radius)
+  // unprojected (lon, lat, radius) and then projects them when forward = true.
+  // When forward = false, the point inputs are assumed to be projected, and then
+  // unprojects them.
   class ProjectPointFunctor : public UnaryReturnSameType {
     GeoReference m_dst;
+    bool m_forward;
 
   public:
-    ProjectPointFunctor(GeoReference const& dst) : m_dst(dst) {}
+    ProjectPointFunctor(GeoReference const& dst, bool forward = true) : m_dst(dst), m_forward(forward) {}
 
     template <class T>
     T operator()(T const& p) const {
       if (p == T()) return p;
       Vector<typename T::value_type,2> lon_lat = m_dst.lonlat_to_point(subvector(p,0,2));
+      typename T::value_type offset = m_dst.datum().radius(lon_lat(0), lon_lat(1));
       return T(lon_lat(0), lon_lat(1),
-               p(2)-m_dst.datum().radius(lon_lat(0), lon_lat(1)));
+               m_forward ? p(2) - offset : p(2) + offset);
     }
   };
 
+  template <class PixelT>
+  class DemToPointImageFunctor : public ReturnFixedType<Vector3> {
+      GeoReference m_georef;
+    public:
+      DemToPointImageFunctor(GeoReference georef) : m_georef(georef) {}
+
+      Vector3 operator()(Vector2 loc, PixelT alt) const {
+        if (is_transparent(alt))
+          return Vector3();
+
+        Vector3 result;
+        subvector(result, 0, 2) = m_georef.point_to_lonlat(m_georef.pixel_to_point(loc));
+        result.z() = alt;
+
+        return result;
+      }      
+  };
 
   /// Takes an ImageView of Vector<ElemT,3> in some source projected space
   /// with (lon,lat,alt) or (x,y,alt) and returns an ImageView of
@@ -84,10 +106,18 @@ namespace cartography {
   // assumes that the source points are [lon, lat, radius] values.
   template <class ImageT>
   UnaryPerPixelView<ImageT, ProjectPointFunctor>
-  inline project_point_image( ImageViewBase<ImageT> const& image, GeoReference const& dst_georef) {
-    return UnaryPerPixelView<ImageT,ProjectPointFunctor>( image.impl(), ProjectPointFunctor(dst_georef) );
+  inline project_point_image( ImageViewBase<ImageT> const& image, GeoReference const& dst_georef, bool forward=true) {
+    return UnaryPerPixelView<ImageT,ProjectPointFunctor>( image.impl(), ProjectPointFunctor(dst_georef, forward) );
   }
 
+  // This utility function converts a DEM to a point image
+  template <class ImageT>
+  BinaryPerPixelView<PixelIndexView, ImageT, DemToPointImageFunctor<typename ImageT::pixel_type> > 
+  inline dem_to_point_image(ImageViewBase<ImageT> const& dem, GeoReference georef) {
+    typedef DemToPointImageFunctor<typename ImageT::pixel_type> func_type;
+    func_type func(georef);
+    return BinaryPerPixelView<PixelIndexView, ImageT, func_type>(PixelIndexView(dem), dem.impl(), func);
+  }
 }} // namespace vw::cartography
 
 #endif // __VW_CARTOGRAPHY_POINTIMAGEMANIPLULATION_H__
