14a87e5db64a56e21021d5b29b66be07924d89ac
Mike Lundy <mike.lundy@nasa.gov>
2009-12-05 11:51:05 +0000

    Add a thread-safe queue.

---
+++ src/vw/Core/ThreadQueue.h
@@ -0,0 +1,83 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+/// \file Core/Queue.h
+///
+/// A thread-safe queue. Uses a condition variable to avoid busy-waiting.
+///
+
+#ifndef __VW_CORE_QUEUE_H__
+#define __VW_CORE_QUEUE_H__
+
+#include <queue>
+#include <vw/Core/FundamentalTypes.h>
+#include <vw/Core/Thread.h>
+
+namespace vw {
+
+template<typename T>
+class ThreadQueue : private boost::noncopyable {
+  private:
+    std::queue<T> m_queue;
+    mutable Mutex m_mutex;
+    Condition m_cond;
+  public:
+    void push(T const& data) {
+      Mutex::Lock lock(m_mutex);
+      m_queue.push(data);
+      lock.unlock();
+
+      // notify AFTER we release the lock, so we don't wake up a thread just to
+      // have it run into a lock
+      m_cond.notify_one();
+    }
+
+    bool empty() const {
+      Mutex::Lock lock(m_mutex);
+      return m_queue.empty();
+    }
+
+    // Try to pop something off, return indicates success
+    bool try_pop(T& data) {
+      Mutex::Lock lock(m_mutex);
+      if(m_queue.empty()) {
+        return false;
+      }
+
+      data = m_queue.front();
+      m_queue.pop();
+      return true;
+    }
+
+    // Wait for data forever
+    void wait_pop(T& data) {
+      Mutex::Lock lock(m_mutex);
+      while(m_queue.empty()) {
+        m_cond.wait(lock);
+      }
+
+      data = m_queue.front();
+      m_queue.pop();
+    }
+
+    // Wait for data with a timeout (in ms)
+    bool timed_wait_pop(T& data, unsigned long duration) {
+      Mutex::Lock lock(m_mutex);
+
+      // Use the predicate to wait until the queue is non-empty (to catch spurious wakeups)
+      if(!m_cond.timed_wait(lock,duration,!boost::bind(&std::queue<T>::empty, boost::cref(m_queue))))
+        return false;
+
+      data = m_queue.front();
+      m_queue.pop();
+      return true;
+    }
+};
+
+} // namespace vw
+
+
+#endif
---
+++ src/vw/Core/tests/TestThreadQueue.cxx
@@ -0,0 +1,74 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+#include <gtest/gtest.h>
+#include <vw/Core/ThreadQueue.h>
+
+using namespace vw;
+
+TEST(ThreadQueue, Basic) {
+  ThreadQueue<uint32> q;
+
+  ASSERT_TRUE(q.empty());
+  for (uint32 i = 0; i < 50; ++i)
+    q.push(i);
+  ASSERT_FALSE(q.empty());
+
+  uint32 pop;
+  for (uint32 i = 0; i < 50; ++i) {
+    ASSERT_FALSE(q.empty());
+    q.wait_pop(pop);
+    EXPECT_EQ(i, pop);
+  }
+}
+
+class PushTask {
+    ThreadQueue<uint32>& m_queue;
+    unsigned m_count, m_value;
+  public:
+    PushTask(ThreadQueue<uint32>& q, uint32 count, uint32 value) : m_queue(q), m_count(count), m_value(value) {}
+    void operator()() {
+      for (uint32 i = 0; i < m_count; ++i) {
+        m_queue.push(m_value);
+      }
+    }
+};
+
+TEST(ThreadQueue, Threaded) {
+  typedef boost::shared_ptr<PushTask>    TheTask;
+  typedef boost::shared_ptr<vw::Thread>  TheThread;
+
+  ThreadQueue<uint32> q;
+  ASSERT_TRUE(q.empty());
+
+  std::vector<std::pair<TheTask, TheThread> > threads(20);
+
+  for (size_t i = 0; i < threads.size(); ++i) {
+    TheTask task(new PushTask(q, 10, i));
+    TheThread thread( new Thread(task) );
+    threads[i] = std::make_pair(task, thread);
+  }
+
+  for (size_t i = 0; i < threads.size(); ++i) {
+    threads[i].second->join();
+  }
+
+  ASSERT_FALSE(q.empty());
+
+  std::vector<uint32> ret(threads.size());
+  uint32 value = ret.size()+1;
+
+  while (!q.empty()) {
+    EXPECT_TRUE(q.timed_wait_pop(value, 0));
+    EXPECT_LT(value, ret.size());
+    ret[value]++;
+  }
+  EXPECT_FALSE(q.timed_wait_pop(value, 0));
+
+  for (size_t i = 0; i < ret.size(); ++i) {
+    EXPECT_EQ(10u, ret[i]);
+  }
+}
