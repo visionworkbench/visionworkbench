6b9d15b37222dcfe42b26e7affed79326f58d7bc
Mike Lundy <mike.lundy@nasa.gov>
2009-12-11 11:48:54 +0000

    transaction_complete() handles the read_cursor now.

--- src/vw/Plate/LocalIndex.cc
+++ src/vw/Plate/LocalIndex.cc
@@ -15,11 +15,23 @@ using namespace vw::platefile;
 #include <fstream>
 #include <boost/filesystem.hpp>
 #include <boost/regex.hpp>
+#include <algorithm>
 namespace fs = boost::filesystem;
 
 // -------------------------------------------------------------------
 //                            LOCAL INDEX
 // -------------------------------------------------------------------
+
+namespace {
+  // The first element in an stl heap (created with std::make_heap) is the
+  // "greatest" value according to the comparison operator. We want that value
+  // to have the smallest transaction id. Thus, this comparison operator is a
+  // strict weak ordering, with the "greater" transaction id in the heap sense
+  // actually being the smallest numerically.
+  bool transaction_sort(const int32& a, const int32& b) {
+    return a > b;
+  }
+}
 
 
 // ------------------------ Private Methods --------------------------
@@ -36,7 +48,7 @@ std::vector<std::string> vw::platefile::LocalIndex::blob_filenames() const {
 
   std::vector<std::string> result;
 
-  if ( !fs::exists( m_plate_filename ) ) 
+  if ( !fs::exists( m_plate_filename ) )
     vw_throw(IOErr() << "LocalIndex::blob_filenames() -- could not open platefile directory.");
 
   // Create a regular expression for matching the pattern 'plate_<number>.blob"
@@ -58,16 +70,16 @@ std::vector<std::string> vw::platefile::LocalIndex::blob_filenames() const {
 // Load index entries by iterating through TileHeaders saved in the
 // blob file.  This function essentially rebuilds an index in memory
 // using entries that had been previously saved to disk.
-void vw::platefile::LocalIndex::load_index(std::string plate_filename, 
+void vw::platefile::LocalIndex::load_index(std::string plate_filename,
                                            std::vector<std::string> const& blob_files) {
 
   std::cout << "\tLoading index: " << plate_filename <<"\n";
 
   for (unsigned int i = 0; i < blob_files.size(); ++i) {
-    // this->log() << "Loading index entries from blob file: " 
+    // this->log() << "Loading index entries from blob file: "
     //             << m_plate_filename << "/" << blob_files[i] << "\n";
-    
-    
+
+
     TerminalProgressCallback tpc(InfoMessage, "\t    " + blob_files[i] + " : ");
     tpc.report_progress(0);
 
@@ -80,7 +92,7 @@ void vw::platefile::LocalIndex::load_index(std::string plate_filename,
       vw_throw(IOErr() << "LocalIndex::load_index() -- could not parse blob number from blob filename.");
     std::string blob_id_str(matches[2].first, matches[2].second);
     int current_blob_id = atoi(blob_id_str.c_str());
-    
+
     Blob blob(m_plate_filename + "/" + blob_files[i], true);
     Blob::iterator iter = blob.begin();
     while (iter != blob.end()) {
@@ -118,7 +130,7 @@ void vw::platefile::LocalIndex::save_index_file() const {
 /// Create a new index.  User supplies a pre-configure blob manager.
 vw::platefile::LocalIndex::LocalIndex( std::string plate_filename, IndexHeader new_index_info) :
   m_plate_filename(plate_filename),
-  m_blob_manager(boost::shared_ptr<BlobManager>( new BlobManager() )), 
+  m_blob_manager(boost::shared_ptr<BlobManager>( new BlobManager() )),
   m_root(boost::shared_ptr<TreeNode<IndexRecord> >( new TreeNode<IndexRecord>() )) {
 
   // Start with the new_index_info, which provides the tile size, file
@@ -133,7 +145,7 @@ vw::platefile::LocalIndex::LocalIndex( std::string plate_filename, IndexHeader n
   // humble purposes.
   srandom(time(0));
   m_header.set_platefile_id(vw::int32(random()));
-  
+
   // Set up the IndexHeader and write it to disk.
   m_header.set_version(VW_PLATE_INDEX_VERSION);
   m_header.set_transaction_read_cursor(0);   // Transaction 0 is the empty mosaic
@@ -148,26 +160,30 @@ vw::platefile::LocalIndex::LocalIndex( std::string plate_filename, IndexHeader n
 }
 
 /// Open an existing index from a file on disk.
-vw::platefile::LocalIndex::LocalIndex(std::string plate_filename) : 
-  m_plate_filename(plate_filename),                                    
+vw::platefile::LocalIndex::LocalIndex(std::string plate_filename) :
+  m_plate_filename(plate_filename),
   m_blob_manager(boost::shared_ptr<BlobManager>( new BlobManager() )),
   m_root(boost::shared_ptr<TreeNode<IndexRecord> >( new TreeNode<IndexRecord>() )) {
 
   // First, check to make sure the platefile directory exists.
-  if ( !fs::exists( plate_filename ) ) 
+  if ( !fs::exists( plate_filename ) )
     vw_throw(IOErr() << "Could not open platefile: \"" << plate_filename << "\" for reading.");
 
   // The load the index file & parse the IndexHeader protobuffer
   std::ifstream ifstr( this->index_filename().c_str() );
-  if (!ifstr.is_open()) 
+  if (!ifstr.is_open())
     vw_throw(IOErr() << "LocalIndex::LocalIndex() -- Could not load index file for reading.");
 
   bool worked = m_header.ParseFromIstream(&ifstr);
   if (!worked)
-    vw_throw(IOErr() << "LocalIndex::LocalIndex() -- Could not parse index information from " 
-             << this->index_filename()); 
+    vw_throw(IOErr() << "LocalIndex::LocalIndex() -- Could not parse index information from "
+             << this->index_filename());
 
   ifstr.close();
+
+  std::make_heap(m_header.mutable_complete_transaction_ids()->begin(),
+                 m_header.mutable_complete_transaction_ids()->end(),
+                 transaction_sort);
 
   // Create the logging facility
   try {
@@ -205,17 +221,17 @@ IndexRecord vw::platefile::LocalIndex::read_request(int col, int row, int depth,
   IndexRecord rec = m_root->search(col, row, depth, transaction_id, exact_transaction_match);
   return rec;
 }
-  
+
 // Writing, pt. 1: Locks a blob and returns the blob id that can
 // be used to write a tile.
-int vw::platefile::LocalIndex::write_request(int size) {  
-  return m_blob_manager->request_lock(size); 
+int vw::platefile::LocalIndex::write_request(int size) {
+  return m_blob_manager->request_lock(size);
 }
 
 // Writing, pt. 2: Supply information to update the index and
 // unlock the blob id.
 void vw::platefile::LocalIndex::write_complete(TileHeader const& header, IndexRecord const& record) {
-  m_blob_manager->release_lock(record.blob_id(), record.blob_offset()); 
+  m_blob_manager->release_lock(record.blob_id(), record.blob_offset());
 
   Mutex::Lock lock(m_mutex);
   m_root->insert(record, header.col(), header.row(), header.depth(), header.transaction_id());
@@ -247,7 +263,7 @@ int32 vw::platefile::LocalIndex::transaction_request(std::string transaction_des
   for (size_t i = 0; i < tile_headers.size(); ++i) {
     IndexRecord empty_record;
     empty_record.set_status(INDEX_RECORD_LOCKED);
-    m_root->insert(empty_record, tile_headers[i].col(), tile_headers[i].row(), 
+    m_root->insert(empty_record, tile_headers[i].col(), tile_headers[i].row(),
                    tile_headers[i].depth(), transaction_id,
                    true );  // insert_at_all_levels = true
   }
@@ -262,7 +278,7 @@ void vw::platefile::LocalIndex::root_complete(int transaction_id,
   Mutex::Lock lock(m_mutex);
 
   for (size_t i = 0; i < tile_headers.size(); ++i) {
-    m_root->clean_branch(tile_headers[i].col(), tile_headers[i].row(), 
+    m_root->clean_branch(tile_headers[i].col(), tile_headers[i].row(),
                          tile_headers[i].depth(), transaction_id);
   }
 }
@@ -270,54 +286,44 @@ void vw::platefile::LocalIndex::root_complete(int transaction_id,
 // Once a chunk of work is complete, clients can "commit" their
 // work to the mosaic by issuing a transaction_complete method.
 void vw::platefile::LocalIndex::transaction_complete(int32 transaction_id) {
+  static const int MAX_OUTSTANDING_TRANSACTION_IDS = 256;
   Mutex::Lock lock(m_mutex);
 
-  // XXXXXX FIXME: The read cursor should only get updated in a more
-  // subtle way (see code below), however we need to find a way of
-  // sorting and deleting transaction cursors from the
-  // complete_transaction_ids in the index_header data structure.
-
-  // Update the transaction read cursor
-  m_header.set_transaction_read_cursor(transaction_id);
-  this->save_index_file();
-
-  // // If this ID is the next one after the current write ID, then we
-  // // increment the read ID.
-  // if (transaction_id == m_header.transaction_read_cursor() + 1) {
-    
-  //   // Update the transaction read cursor
-  //   m_header.set_transaction_read_cursor(transaction_id);
-  //   this->save_index_file();
-
-  //   // Replay the remaining transaction IDs to see if the next ids are outstanding.
-  //   bool match;
-  //   do {
-  //     match = false;
-  //     google::protobuf::RepeatedField<const int32>::iterator iter = m_header.complete_transaction_ids().begin();
-  //     while (iter != m_header.complete_transaction_ids().end()) {
-  //       if (*iter == m_header.transaction_read_cursor() + 1) {
-  //         m_header.set_transaction_read_cursor(*iter);
-  //         this->save_index_file();
-  //         match = true;
-  //       }
-  //       ++iter;
-  //     }
-  //   } while (match);
-
-  // } else {
-
-  //   // This wasn't the next ID.  We store it in the queue and save it for later.
-  //   m_header.mutable_complete_transaction_ids()->Add(transaction_id);
-  //   this->save_index_file();
-
-  //   const int MAX_OUTSTANDING_TRANSACTION_IDS = 10;
-  //   if (m_header.complete_transaction_ids().size() > MAX_OUTSTANDING_TRANSACTION_IDS) 
-  //     vw_out(0) << "WARNING: Detected potential stall in the transaction pipeline.  "
-  //               << "There are " << m_header.complete_transaction_ids().size() 
-  //               << " completed IDs waiting in the queue.\n";
-  // }
+  // We handle the incoming id without inserting it in the heap, if we can (in
+  // order to avoid a re-heapify)
   this->log() << "Transaction " << transaction_id << " complete.\n";
 
+  // Is this the next transaction we're waiting for?
+  if (transaction_id == m_header.transaction_read_cursor() + 1) {
+    // It is! Update the transaction read cursor
+    m_header.set_transaction_read_cursor(transaction_id);
+  } else {
+    // It isn't. Remember it, reheapify, check queue depth, and bail out.
+    m_header.add_complete_transaction_ids(transaction_id);
+    std::push_heap(m_header.mutable_complete_transaction_ids()->begin(),
+                   m_header.mutable_complete_transaction_ids()->end(),
+                   transaction_sort);
+
+    if (m_header.complete_transaction_ids_size() > MAX_OUTSTANDING_TRANSACTION_IDS) {
+      vw_out(vw::WarningMessage) << "Potential stall in transaction pipeline. "
+                                 << m_header.complete_transaction_ids_size()
+                                 << " are waiting" << std::endl;
+    }
+    return;
+  }
+
+  // Okay. We completed one- perhaps more are ready?
+  while (m_header.complete_transaction_ids_size() > 0 &&
+         m_header.complete_transaction_ids(0) == m_header.transaction_read_cursor() + 1) {
+    m_header.set_transaction_read_cursor(m_header.complete_transaction_ids(0));
+    std::pop_heap(m_header.mutable_complete_transaction_ids()->begin(),
+                  m_header.mutable_complete_transaction_ids()->end(),
+                  transaction_sort);
+    m_header.mutable_complete_transaction_ids()->RemoveLast();
+  }
+
+  // If we got this far. we updated the read cursor at least once, so save it.
+  this->save_index_file();
 }
 
 // Once a chunk of work is complete, clients can "commit" their
