182daffd9af4b13fd527de91f198ce91b7289e4b
Zack Moratto <zachary.m.moratto@nasa.gov>
2009-12-15 07:59:27 +0000

    Unbreaking VW. Moving Scaled Gradient descriptor arrays.
    Static arrays must be defined in CC file.

---
+++ src/vw/InterestPoint/Descriptor.cc
@@ -0,0 +1,21 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+/// \file Descriptor.cc
+///
+/// Basic classes and structures for storing image interest points.
+///
+#include <vw/InterestPoint/Descriptor.h>
+
+namespace vw {
+namespace ip {
+
+  const uint SGradDescriptorGenerator::box_strt[5] = {17,15,9,6,0};
+  const uint SGradDescriptorGenerator::box_size[5] = {2,4,8,10,14};
+  const uint SGradDescriptorGenerator::box_half[5] = {1,2,4,5,7};
+
+}} // namespace vw::ip
