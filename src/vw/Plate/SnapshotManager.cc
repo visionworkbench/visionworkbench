124540a9cd11c9a16d31b3e70d1a3f01b81a33a3
Michael Broxton <michael.broxton@nasa.gov>
2010-02-17 11:58:09 +0000

    Supersampling of low res tiles returns to snapshotting thanks to a fancy new recursive implementation of the core snapshotting routine.  Still need to optimize this sucker, but the basic functionality is here and it seems to work.

--- src/vw/Plate/SnapshotManager.cc
+++ src/vw/Plate/SnapshotManager.cc
@@ -9,6 +9,186 @@
 #include <vw/Plate/SnapshotManager.h>
 #include <vw/Mosaic/ImageComposite.h>
 #include <vw/Image/ImageView.h>
+#include <set>
+
+namespace vw {
+namespace platefile {
+
+  template <class PixelT> 
+  void snapshot_helper(int current_col, 
+                       int current_row, 
+                       int current_level, 
+                       std::map<int32, TileHeader> composite_tiles,
+                       boost::shared_ptr<PlateFile> platefile,
+                       vw::BBox2i const& target_region,
+                       int target_level, 
+                       int start_transaction_id, 
+                       int end_transaction_id, 
+                       int write_transaction_id) {
+
+    // Check to see if there are any tiles at this level that need to be
+    // snapshotted.  We fetch one additional tile outside of the
+    // specified range so that we get any tiles that were not actually
+    // composited during the last snapshot.
+    std::list<TileHeader> tile_records;
+    try {
+      tile_records = platefile->search_by_location(current_col, current_row, current_level,
+                                                   start_transaction_id, end_transaction_id,
+                                                   true); // fetch_one_additional_entry
+    } catch (TileNotFoundErr &e) {
+      /* do nothing */
+    }
+
+    // If there are no valid tiles at this level, then there is nothing
+    // further for us to do here on this branch of the recursion.  We
+    // return immediately.
+    if (tile_records.size() == 0)
+      return;
+
+    // Insert the TileHeaders for this level into the parent_tiles
+    // map.  Elements will be inserted in order, and any duplicate
+    // transaction_ids will be overwritten by new tile records.
+    for (std::list<TileHeader>::iterator iter = tile_records.begin();
+         iter != tile_records.end(); ++iter) {
+      composite_tiles[iter->transaction_id()] = *iter;
+    }
+
+    // If we have reached the target level, then we use the
+    // accumulated tiles to generate a snapshot for this tile.
+    if (current_level == target_level) {
+
+      // Create a tile into which we will accumulate composited data.
+      ImageView<PixelT> composite_tile(platefile->default_tile_size(),
+                                       platefile->default_tile_size());
+
+      std::cout << "COMPOSITING " << current_col << " " << current_row 
+                << " @ " << current_level << " / " << target_level << "\n";
+
+      int num_composited = 0;
+      for (std::map<int32, TileHeader>::reverse_iterator iter = composite_tiles.rbegin(); 
+           iter != composite_tiles.rend(); ++iter) {
+
+        TileHeader &current_hdr = iter->second;
+
+        std::cout << "\t--> [ " << current_hdr.transaction_id() << " ]  " 
+                  << current_hdr.col() << " " << current_hdr.row()
+                  << " @ " << current_hdr.level() << "\n";
+        
+        // Read the tile from the platefile.
+        ImageView<PixelT> new_tile;
+        try {
+          platefile->read(new_tile, 
+                          current_hdr.col(), current_hdr.row(), 
+                          current_hdr.level(), current_hdr.transaction_id(),
+                          true); // exact_transaction_match
+        } catch (BlobIoErr &e) {
+          // If we get a BlobIO error, that's bad news, but not worth
+          // killing the snapshot for.  Instead we log the error here and move
+          // onto the next location.
+          std::ostringstream ostr;
+          ostr << "WARNING: error reading tile from blob: " << e.what();
+          platefile->log(ostr.str());
+          continue;
+        }
+        
+        // If we are compositing a tile from a lower resolution in the
+        // pyramid, it needs to be supersampled and cropped before we
+        // can use it here.
+        if (current_hdr.level() != target_level) {
+          
+          int level_diff = target_level - current_hdr.level();
+          int scaling_factor = pow(2,level_diff);
+          int subtile_u = current_col - current_hdr.col() * scaling_factor;
+          int subtile_v = current_row - current_hdr.row() * scaling_factor;
+          BBox2i subtile_bbox(new_tile.cols() * subtile_u,
+                              new_tile.rows() * subtile_v,
+                              new_tile.cols(), new_tile.rows());
+        
+          // Scale up and interpolate the old_tile, then crop out the
+          // subtile that we need.
+          ImageView<PixelT> subtile = 
+            crop(transform(new_tile, ResampleTransform(scaling_factor, scaling_factor), 
+                           ConstantEdgeExtension(), BilinearInterpolation()),
+                 subtile_bbox);
+          
+          // Replace the old data
+          new_tile = subtile;
+        }
+        
+
+        // If this is the first tile in the location, and it is opaque
+        // (which is a very common case), then we don't need to save
+        // it as part of the snapshot since its already on top.  This
+        // optimization saves us both space and time!
+        if ( is_opaque(new_tile) && 
+             iter == composite_tiles.rbegin() && 
+             current_hdr.level() == target_level) 
+          break;
+
+        // Add the new tile UNDERNEATH the tiles we have already composited.
+        vw::mosaic::ImageComposite<PixelT> composite;
+        composite.insert(new_tile, 0, 0);
+        composite.insert(composite_tile, 0, 0);
+        composite.set_draft_mode( true );
+        composite.prepare();
+
+        // Overwrite the composite_tile
+        composite_tile = composite;
+        ++num_composited;
+
+        // Check to see if the composite tile is opaque.  If it is,
+        // then there's no point in compositing any more tiles because
+        // they will end up hidden anyway.
+        if ( is_opaque(composite_tile) )
+          break;
+      }
+
+      if (num_composited > 1) {
+        // -- debug
+        // std::cout << "Compositing " << header_current_hdr.col() << " " 
+        //           << header_current_hdr.row() << " @ " << header_current_hdr.level() << "  [ "
+        //           << tiles_at_location.size() << "]\n";
+        //---
+
+        platefile->write_update(composite_tile, 
+                                current_col, current_row, current_level,
+                                write_transaction_id);
+      }
+      return;
+
+    } else {
+
+      // There are four possible children that we can explore for more
+      // snapshotting information.  
+      for (int new_row = current_row*2; new_row < current_row*2+2; ++new_row) {
+        for (int new_col = current_col*2; new_col < current_col*2+2; ++new_col) {
+
+          // We should only explore those child tiles that overlap
+          // with the target_region at the target level.  To test
+          // this, we create a bounding box for the child tile we are
+          // testing, and scale that bounding box up to see how big it
+          // is when projected into the target_level of the pyramid.
+          BBox2i parent_region(new_col, new_row, 1, 1);
+          parent_region.min() *= pow(2,target_level-(current_level+1));
+          parent_region.max() *= pow(2,target_level-(current_level+1));
+
+
+          // With the scaled up parent region and the target region in
+          // hand, we can do a simple intersection test to assess
+          // whether this child tile is worth exploring.
+          if (parent_region.intersects(target_region)) {
+            snapshot_helper<PixelT>(new_col, new_row, current_level+1,
+                                    composite_tiles, platefile, target_region, target_level, 
+                                    start_transaction_id, end_transaction_id, 
+                                    write_transaction_id);
+          }
+        }
+      }
+
+    }
+  }
+}} // namespace vw::platefile
+
 
 // mipmap() generates mipmapped (i.e. low resolution) tiles in the mosaic.
 template <class PixelT>
@@ -22,6 +202,14 @@ void vw::platefile::SnapshotManager<PixelT>::snapshot(int level, BBox2i const& t
   std::list<BBox2i> tile_workunits = bbox_tiles(tile_region, 1024, 1024);
   for ( std::list<BBox2i>::iterator region_iter = tile_workunits.begin(); 
         region_iter != tile_workunits.end(); ++region_iter) {
+
+    // Create an empty list of composite tiles and then kick off the
+    // recursive snapshotting process.
+    std::map<int32, TileHeader> composite_tiles;
+    snapshot_helper<PixelT>(0, 0, 0, composite_tiles, m_platefile, *region_iter, level, 
+                            start_transaction_id, end_transaction_id, write_transaction_id);
+    return;
+                            
 
     // It will save us time (and result in fewer RPC calls to
     // valid_tiles()) if we start the search by popping up several
@@ -61,6 +249,8 @@ void vw::platefile::SnapshotManager<PixelT>::snapshot(int level, BBox2i const& t
       search_level++;
     }
 
+    // It is only worth processing this region if there were tiles at
+    // higher levels of the pyramid that also need to be mipmapped.
     // If our search at higher levels of the pyramid failed, then we
     // skip this region.  This optimization prevents us from wasting
     // time looking for valid tiles in an area of the mosaic that is
@@ -75,7 +265,7 @@ void vw::platefile::SnapshotManager<PixelT>::snapshot(int level, BBox2i const& t
                                                                           true); // fetch_one_additional_entry
 
     // If there were no valid tiles at *this* level, then we can
-    // continue also.
+    // continue without further processing.
     if (tiles_in_region.size() == 0) 
       continue;
 
@@ -207,7 +397,7 @@ void vw::platefile::SnapshotManager<PixelT>::full_snapshot(int start_transaction
   for (int level = 0; level < m_platefile->num_levels(); ++level) {    
 
     // For debugging:
-    //  for (int level = 0; level < 1; ++level) {    
+    //  for (int level = 0; level < 2; ++level) {    
 
     // Snapshot the entire region at each level.  These region will be
     // broken down into smaller work units in snapshot().
@@ -220,6 +410,7 @@ void vw::platefile::SnapshotManager<PixelT>::full_snapshot(int start_transaction
                                              subdivided_region_size);
     for ( std::list<BBox2i>::iterator region_iter = workunits.begin(); 
           region_iter != workunits.end(); ++region_iter) {
+      std::cout << "\n\nSNAPSHOTTING : " << level << " " << *region_iter  << "\n";
       snapshot(level, *region_iter, start_transaction_id, 
                end_transaction_id, write_transaction_id);
     }
