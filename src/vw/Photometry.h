a4bced7bffb65970d26bf8d75169525393a437a1
Ara Nefian <ara.nefian@nasa.gov>
2010-03-18 03:57:34 +0000

    added Photometry.h

---
+++ src/vw/Photometry.h
@@ -0,0 +1,29 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+/// \file Photometry.h
+/// 
+/// A convenience header that includes the header files in vw/Photometry.
+/// 
+#ifndef __VW_PHOTOMETRY_H__
+#define __VW_PHOTOMETRY_H__
+
+#include <vw/Photometry/Albedo.h>
+#include <vw/Photometry/Camres.h>
+#include <vw/Photometry/Exposure.h>
+#include <vw/Photometry/Index.h>
+#include <vw/Photometry/Misc.h>
+#include <vw/Photometry/Outlier.h>
+#include <vw/Photometry/Reconstruct.h>
+#include <vw/Photometry/Reflectance.h>
+#include <vw/Photometry/Shadow.h>
+#include <vw/Photometry/Shape.h>
+#include <vw/Photometry/ShapeFromShading.h>
+#include <vw/Photometry/Weights.h>
+
+#endif // __VW_PHOTOMETRY_H__
+ 
