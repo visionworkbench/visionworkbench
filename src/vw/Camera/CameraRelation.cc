4cfaaab07599976850627a9eb91a609b0d542c68
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-03-22 06:12:36 +0000

    Submit idea for Camera Relations Network
    This is the start of more formal development of the Relations network that was developed in ASP. This will act as a fast intermediate form of the Control Network that is usefully for building cnets and for building sparse hessians.

---
+++ src/vw/Camera/CameraRelation.cc
@@ -0,0 +1,137 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2010 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+/// \file CameraRelation.cc
+///
+
+#include <vw/Camera/CameraRelation.h>
+
+namespace vw {
+namespace camera {
+
+  // Implementations for specific features
+  ControlMeasure
+  IPFeature::control_measure() const {
+    return ControlMeasure( m_ip.x, m_ip.y,
+                           m_ip.scale, m_ip.scale,
+                           m_camera_id );
+  }
+
+  ControlMeasure
+  JFeature::control_measure() const {
+    return ControlMeasure( m_location[0], m_location[1],
+                           m_scale[0],    m_scale[1],
+                           m_point_id );
+  }
+
+  // Camera Relation Network
+  template <class FeatureT>
+  void CameraRelationNetwork<FeatureT>::add_node( cnode const& node ) {
+    m_nodes.push_back( node );
+  }
+
+  template <class FeatureT>
+  void CameraRelationNetwork<FeatureT>::read_controlnetwork( ControlNetwork const& /*cnet*/ ) {
+    vw_throw( NoImplErr() << "Not Finished Yet!" );
+  }
+
+  template <class FeatureT>
+  void CameraRelationNetwork<FeatureT>::write_controlnetwork( ControlNetwork & cnet ) const {
+
+    if ( this->size() == 0 )
+      vw_throw( ArgumentErr() << "CameraRelation network is empty." );
+    cnet.clear();
+
+    // This process is destructive, making a copy of self.
+    CameraRelationNetwork<FeatureT> crn = (*this);
+
+    // On top of building the control network, we're also going filter
+    // out 'spiral' type errors. Features that managed to link to the
+    // image twice.
+    int spiral_error_count = 0;
+    {
+      TerminalProgressCallback progress("asp","Assembly:  ");
+      progress.report_progress(0);
+      for ( uint32 i = 0; i < crn.size() - 1; i++ ) {
+        progress.report_progress(float(i)/float(crn.size()-1));
+        typedef boost::shared_ptr<FeatureT> f_ptr;
+        typedef typename std::list<f_ptr>::iterator f_list_iter;
+
+        // Iterating over matched relations inside a camera node and
+        // building control points for them.
+        for ( f_list_iter iter = crn[i].begin();
+              iter != crn[i].end(); iter++ ) {
+          // 1.) Building a listing of interest point for a control
+          // point
+          std::list<f_ptr> interestpts;
+          interestpts.push_back(*iter);
+          (*iter)->list_connections( interestpts );
+
+          ControlPoint cpoint( ControlPoint::TiePoint );
+
+          // 2.) Adding this location
+          cpoint.add_measure( (*iter)->control_measure() );
+
+          // 3.) Adding and Removing measures in all other locations
+          f_list_iter measure = interestpts.begin();
+          measure++;
+          for ( ; measure != interestpts.end(); measure++ ) {
+            crn[(*measure)->m_camera_id].relations.remove(*measure);
+            cpoint.add_measure( (*measure)->control_measure() );
+          }
+
+          // 4.) Removing this location finally
+          iter = crn[i].relations.erase(iter);
+          iter--;
+
+          // 5.) Checking for spiral error
+          {
+            std::list<unsigned> previous_camera;
+            bool match = false;
+            for ( f_list_iter interest = interestpts.begin();
+                  interest != interestpts.end(); interest++ ) {
+              for ( std::list<unsigned>::iterator previous = previous_camera.begin();
+                    previous != previous_camera.end(); previous++ ) {
+                if ((*previous) == (*interest)->m_camera_id) {
+                  match = true;
+                  break;
+                }
+              }
+              previous_camera.push_back( (*interest)->m_camera_id );
+              if ( match ) {
+                continue;
+              }
+            }
+            if ( match ) {
+              spiral_error_count++;
+              continue;
+            }
+          }
+
+          // 6.) Did you pass? Sweet you're in the gang!
+          if ( cpoint.size() > 1 )
+            cnet.add_control_point( cpoint );
+        } // end of iteration over relations
+      } // end of iteration over camera nodes
+      progress.report_finished();
+      if ( spiral_error_count != 0 )
+        vw_out(WarningMessage,"camera") << "\t"
+                                        << spiral_error_count
+                                        << " control points removed due to spiral errors.\n";
+    }
+    VW_ASSERT( cnet.size() != 0,
+               Aborted() << "Failed to load any points, Control Network empty\n" );
+  }
+
+  // Explicit template Instantiation
+#define VW_INSTANTIATE_CAMERA_RELATION_TYPES(FEATURET) \
+  template class CameraRelationNetwork<FEATURET >; \
+
+  VW_INSTANTIATE_CAMERA_RELATION_TYPES(IPFeature)
+  VW_INSTANTIATE_CAMERA_RELATION_TYPES(JFeature)
+
+}}
---
+++ src/vw/Camera/CameraRelation.h
@@ -0,0 +1,160 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2009 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+// Camera Relations Network
+//
+// This is not a replacement for the Control Network, but instead a
+// different approach of viewing the same data. It's optimized in a way
+// to allow for fast insertions of match data, it's great for organizing
+// jacobians right before insertion into hessians, and it also provides a
+// constant structure for handling internal variables inside of bundle
+// adjustment.
+//
+// Control Networks are still useful for providing an organized
+// manner for saving data and for interfacing with ISIS. Control Networks
+// are also easier to understand at first glance. A control network can
+// be created from a camera relations network and the opposite is also
+// true.
+
+#ifndef __VW_CAMERA_RELATIONS_H__
+#define __VW_CAMERA_RELATIONS_H__
+
+#include <vw/Math/Matrix.h>
+#include <vw/Camera/ControlNetwork.h>
+#include <vw/InterestPoint/InterestData.h>
+
+namespace vw {
+namespace camera {
+
+  // Feature Base
+  template <class ImplT>
+  struct FeatureBase {
+
+    FeatureBase( uint32 id ) : m_camera_id(id) {}
+
+    // Access to derived type
+    inline ImplT& impl() { return static_cast<ImplT&>(*this); }
+    inline ImplT const& impl() const { return static_cast<ImplT const&>(*this); }
+
+    typedef boost::shared_ptr<ImplT> f_ptr;
+    std::list<f_ptr> m_connections;
+    uint32 m_camera_id;
+
+    // Returns a string identifying feature type
+    std::string type() { return impl().type(); }
+    // Connects this feature to another
+    void connection( f_ptr& con, bool check=true ) {
+      if (check) {
+        typedef typename std::list<f_ptr>::iterator list_it;
+        for ( list_it iter = m_connections.begin();
+              iter != m_connections.end(); iter++ ) {
+          if ( **iter == *con )
+            return;
+        }
+      }
+      m_connections.push_back( con );
+    }
+    // List all features connected to this one. This will traverse all
+    // connections and append them to the provided list.
+    void list_connections( std::list<f_ptr>& listing ) {
+      typedef typename std::list<f_ptr>::iterator list_it;
+      for ( list_it iter = m_connections.begin();
+            iter != m_connections.end(); iter++ ) {
+        bool contains = false;
+        for ( list_it diter = listing.begin();
+              diter != listing.end(); diter++ )
+          if ( *diter == *iter ) {
+            contains = true;
+            break;
+          }
+
+        if ( !contains ) {
+          listing.push_back(*iter);
+          (*iter)->list_connections( listing );
+        }
+      }
+    }
+    // Interface to create Control Measure
+    ControlMeasure control_measure() const { return impl().control_measure(); }
+  };
+
+  // Interest Point Feature
+  // - Intended for fast insertion of IP matches
+  struct IPFeature : public FeatureBase<IPFeature> {
+    typedef boost::shared_ptr<IPFeature> f_ptr;
+    ip::InterestPoint m_ip;
+
+    IPFeature ( ip::InterestPoint const& ip,
+                uint32 const& id ) :
+    FeatureBase<IPFeature>(id), m_ip(ip) {}
+    std::string type() { return "IP"; }
+    ControlMeasure control_measure() const;
+  };
+
+  // Jacobian Feature
+  // - Intended for internal use in BA
+  struct JFeature : public FeatureBase<JFeature> {
+    typedef boost::shared_ptr<JFeature> f_ptr;
+    Matrix<double> m_a_jacobian;
+    Matrix<double> m_b_jacobian;
+    uint32 m_point_id;
+    Vector2f m_location;
+    Vector2f m_scale;
+
+    JFeature ( uint32 const& i, uint32 const& j ) :
+    FeatureBase<JFeature>(j), m_point_id(i) {}
+    std::string type() { return "J"; }
+    ControlMeasure control_measure() const;
+  };
+
+  // Camera Relation Class
+  template <class FeatureT>
+  struct CameraNode {
+    typedef boost::shared_ptr<FeatureT> f_ptr;
+    int id;
+    std::string name;
+    std::list<f_ptr> relations;
+
+    CameraNode ( int tid, std::string tname ) :
+    id(tid), name(tname) {}
+
+    // Iterator access (saves a section for me the monkey)
+    typedef typename std::list<f_ptr>::iterator iterator;
+    typedef typename std::list<f_ptr>::const_iterator const_iterator;
+    iterator begin() { return relations.begin(); }
+    iterator end()   { return relations.end();   }
+    const_iterator begin() const { return relations.begin(); }
+    const_iterator end()   const { return relations.end();   }
+  };
+
+  // Base of the Camera Relation Network
+  template <class FeatureT>
+  class CameraRelationNetwork {
+    typedef CameraNode<FeatureT> cnode;
+    std::vector<cnode> m_nodes;
+
+  public:
+
+    // Iterator access
+    typedef typename std::vector<cnode>::iterator iterator;
+    typedef typename std::vector<cnode>::const_iterator const_iterator;
+    uint32 size( void ) const { return m_nodes.size(); }
+    cnode& operator[]( int32 const& i ) { return m_nodes[i]; }
+    iterator begin() { return m_nodes.begin(); }
+    iterator end()   { return m_nodes.end();   }
+    const_iterator begin() const { return m_nodes.begin(); }
+    const_iterator end()   const { return m_nodes.end();   }
+
+    // Complex functions
+    void add_node( cnode const& node );
+    void read_controlnetwork( ControlNetwork const& cnet );
+    void write_controlnetwork( ControlNetwork & cnet ) const;
+
+  };
+
+}}
+
+#endif//__VW_CAMERA_RELATIONS_H__
