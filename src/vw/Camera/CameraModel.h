3d8ba9f4170634a40704bcc00ad348f8bc148488
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-06-20 21:51:28 -0700

    Whitespace cleanup CameraModel.h
    It was looking ugly. Also there was duplicates of our license.

--- src/vw/Camera/CameraModel.h
+++ src/vw/Camera/CameraModel.h
@@ -4,42 +4,18 @@
 // All Rights Reserved.
 // __END_LICENSE__
 
-
-// __BEGN_LICENSE__
-// 
-// Copyright (C) 2006 United States Government as represented by the
-// Administrator of the National Aeronautics and Space Administration
-// (NASA).  All Rights Reserved.
-// 
-// Copyright 2006 Carnegie Mellon University. All rights reserved.
-// 
-// This software is distributed under the NASA Open Source Agreement
-// (NOSA), version 1.3.  The NOSA has been approved by the Open Source
-// Initiative.  See the file COPYING at the top of the distribution
-// directory tree for the complete NOSA document.
-// 
-// THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
-// KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
-// LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
-// SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
-// A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
-// THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
-// DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
-// 
-// __END_LICENSE__
-
 /// \file CameraModel.h
-/// 
+///
 /// This file contains the abstract base class from which all camera
 /// models must derive their interface.
-/// 
+///
 #ifndef __VW_CAMERA_CAMERAMODEL_H__
 #define __VW_CAMERA_CAMERAMODEL_H__
 
 #include <fstream>
 #include <vw/Math/Quaternion.h>
 
-namespace vw { 
+namespace vw {
 namespace camera {
 
   /// This is the abstract base class for a camera model object.  You
@@ -105,7 +81,7 @@ namespace camera {
       return Quaternion<double>();
     }
 
-  };  
+  };
 
 
   /// This class is useful if you have an existing camera model, and
@@ -113,12 +89,12 @@ namespace camera {
   /// (position and pose).  This is particularly useful in Bundle
   /// Adjustment.
   class AdjustedCameraModel : public CameraModel {
-    
+
     boost::shared_ptr<CameraModel> m_camera;
     Vector3 m_translation;
     Quaternion<double> m_rotation;
     Quaternion<double> m_rotation_inverse;
-    
+
   public:
     AdjustedCameraModel(boost::shared_ptr<CameraModel> camera_model) : m_camera(camera_model) {
       m_rotation = math::Quaternion<double>(math::identity_matrix<3>());
@@ -126,7 +102,7 @@ namespace camera {
     }
 
     AdjustedCameraModel(boost::shared_ptr<CameraModel> camera_model,
-                        Vector3 const& translation, math::Quaternion<double> const& rotation) : 
+                        Vector3 const& translation, math::Quaternion<double> const& rotation) :
       m_camera(camera_model), m_translation(translation), m_rotation(rotation), m_rotation_inverse(inverse(rotation)) {}
 
     virtual ~AdjustedCameraModel() {}
@@ -163,7 +139,7 @@ namespace camera {
     }
 
     void set_translation(Vector3 const& translation) { m_translation = translation; }
-    void set_rotation(Quaternion<double> const& rotation) { 
+    void set_rotation(Quaternion<double> const& rotation) {
       m_rotation = rotation;
       m_rotation_inverse = inverse(m_rotation);
     }
@@ -173,7 +149,7 @@ namespace camera {
     }
 
     void set_axis_angle_rotation(Vector3 const& axis_angle) {
-      Quaternion<double> rot; 
+      Quaternion<double> rot;
       double angle = norm_2(axis_angle);
       Vector3 temp = normalize(axis_angle);
       double s = sin(angle/2);
@@ -205,7 +181,7 @@ namespace camera {
     }
 
     virtual Quaternion<double> camera_pose(Vector2 const& pix) const {
-      return m_rotation*m_camera->camera_pose(pix);      
+      return m_rotation*m_camera->camera_pose(pix);
     }
 
     void write(std::string filename) {
@@ -234,6 +210,6 @@ namespace camera {
   /// from the camera center.
   VW_DEFINE_EXCEPTION(PixelToRayErr, vw::Exception);
 
-}}	// namespace vw::camera
+}} // namespace vw::camera
 
 #endif // __VW_CAMERA_CAMERAMODEL_H__
