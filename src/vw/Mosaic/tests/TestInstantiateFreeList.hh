cad4024a44adc8ecf296e0099c16e320ca257059
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-07-01 16:24:07 -0700

    Removed all TestInstantiate*
    This code was mostly commented out.

--- src/vw/Mosaic/tests/TestInstantiateFreeList.hh
+++
@@ -1,8 +0,0 @@
-// __BEGIN_LICENSE__
-// Copyright (C) 2006-2010 United States Government as represented by
-// the Administrator of the National Aeronautics and Space Administration.
-// All Rights Reserved.
-// __END_LICENSE__
-
-
-template vw::ImageView<PixelT> vw::mosaic::box_subsample < PixelT > (const vw::ImageView<PixelT>&, const vw::Vector2i&);
