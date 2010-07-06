f8adff33abf415a9a50f8001d422514ece264c85
Zack Moratto <zachary.m.moratto@nasa.gov>
2010-07-05 22:36:21 -0700

    Added Interpolation Test for how I wished it worked
    It's my wish that if the underlining image is floating point accessible, then interpolationview should perform no effect. This would mean also passing traits.
    
    There's also a check to make sure that prerasterization doesn't happen if input is floating point accessible.

--- src/vw/Image/tests/TestInterpolation.cxx
+++ src/vw/Image/tests/TestInterpolation.cxx
@@ -79,3 +79,68 @@ TEST( Interpolation, Nearest ) {
   ASSERT_FALSE( bool_trait<IsMultiplyAccessible>( interpolate(im, NearestPixelInterpolation()) ) );
 }
 
+template <class PixelT>
+class FloatingView : public ImageViewBase<FloatingView<PixelT> > {
+  int32 m_cols, m_rows, m_planes;
+public:
+  typedef PixelT pixel_type;
+  typedef PixelT result_type;
+  typedef ProceduralPixelAccessor<FloatingView> pixel_accessor;
+
+  FloatingView( int32 cols, int32 rows, int32 planes = 1 )
+    : m_cols(cols), m_rows(rows), m_planes(planes) {}
+
+  inline int32 cols() const { return m_cols; }
+  inline int32 rows() const { return m_rows; }
+  inline int32 planes() const { return m_planes; }
+
+  inline pixel_accessor origin() const { return pixel_accessor( *this ); }
+
+  inline result_type operator()( double col, double row, double /*plane*/=0 ) const { return col*row; }
+
+  typedef FloatingView prerasterize_type;
+  inline prerasterize_type prerasterize( BBox2i /*bbox*/ ) const { return *this; }
+  template <class DestT> inline void rasterize( DestT const& dest, BBox2i bbox ) const {
+    vw::rasterize( prerasterize(bbox), dest, bbox );
+  }
+};
+
+namespace vw {
+template <class PixelT>
+struct IsMultiplyAccessible<FloatingView<PixelT> > : public true_type {};
+
+template <class PixelT>
+struct IsFloatingPointIndexable<FloatingView<PixelT> > : public true_type {};
+}
+
+TEST( Interpolation, DISABLED_PassThrough ) {
+  typedef InterpolationView<FloatingView<float>, BilinearInterpolation> bilinear_view;
+  typedef InterpolationView<FloatingView<float>, BicubicInterpolation> bicubic_view;
+  typedef InterpolationView<FloatingView<float>, NearestPixelInterpolation> near_view;
+  // If underlining image is floating point accessible, then
+  // InterpolationView should pass through all traits and access.
+  EXPECT_TRUE( bool_trait<IsFloatingPointIndexable>(
+          bilinear_view(FloatingView<float>(5,5),BilinearInterpolation())) );
+  EXPECT_TRUE( bool_trait<IsMultiplyAccessible>(
+          bilinear_view(FloatingView<float>(5,5),BilinearInterpolation())) );
+  EXPECT_TRUE( bool_trait<IsFloatingPointIndexable>(
+          bicubic_view(FloatingView<float>(5,5),BicubicInterpolation())) );
+  EXPECT_TRUE( bool_trait<IsMultiplyAccessible>(
+          bicubic_view(FloatingView<float>(5,5),BicubicInterpolation())) );
+  EXPECT_TRUE( bool_trait<IsFloatingPointIndexable>(
+          near_view(FloatingView<float>(5,5),NearestPixelInterpolation())) );
+  EXPECT_TRUE( bool_trait<IsMultiplyAccessible>(
+          near_view(FloatingView<float>(5,5),NearestPixelInterpolation())) );
+
+  // Verify that InterpolationView really isn't doing anything to mess
+  // this up. However doesn't work for Bilinear and Bicubic.
+  EXPECT_EQ( 0.25, (near_view(FloatingView<float>(5,5),
+                              NearestPixelInterpolation())(0.5,0.5)) );
+
+  // Verify that prerasterization is not happening for interpolations
+  // of already floating point accessible images.
+  near_view::prerasterize_type pre =
+    near_view(FloatingView<float>(5,5),
+              NearestPixelInterpolation()).prerasterize(BBox2i(0,0,5,5));
+  EXPECT_EQ( 0.25, pre(0.5,0.5) );
+}
