c9a517721d7ab105b93b4ded5d1c6fa963f2bec6
Mike Lundy <mike@fluffypenguin.org>
2010-11-29 15:18:23 -0800

    increase imagecomposite test complexity

--- src/vw/Mosaic/tests/TestImageComposite.cxx
+++ src/vw/Mosaic/tests/TestImageComposite.cxx
@@ -6,8 +6,8 @@ using namespace vw;
 using namespace vw::mosaic;
 
 ImageView<uint32> make(uint32 x) {
-  ImageView<uint32> img(1,1);
-  img(0,0) = x;
+  ImageView<uint32> img(8,8);
+  fill(img, x);
   return img;
 }
 
@@ -16,13 +16,19 @@ TEST(TestImageComposite, Basic) {
   c.set_draft_mode(true);
 
   c.insert(make(2), 3, 0);
-  EXPECT_EQ(1, c.rows());
-  EXPECT_EQ(1, c.cols());
+  EXPECT_EQ(8, c.rows());
+  EXPECT_EQ(8, c.cols());
   EXPECT_EQ(1, c.planes());
 
   c.insert(make(3), 0, 0);
-  EXPECT_EQ(1, c.rows());
-  EXPECT_EQ(4, c.cols());
+  EXPECT_EQ(8, c.rows());
+  EXPECT_EQ(11, c.cols());
   EXPECT_EQ(1, c.planes());
 
+  for (int32 row = 0; row < 8; ++row) {
+    for (int32 col = 0; col < 8; ++col)
+      EXPECT_EQ(3, c(col, row)) << "at (" << col << "," << row << ")";
+    for (int32 col = 8; col < 11; ++col)
+      EXPECT_EQ(2, c(col, row)) << "at (" << col << "," << row << ")";
+  }
 }
