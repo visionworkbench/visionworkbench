264286d183569e94e561dc5c2f446588007d7a3b
Mike Lundy <mike@fluffypenguin.org>
2011-05-10 15:05:07 -0700

    overthink the image2qtree test script

--- src/test/image2qtree_test.py
+++ src/test/image2qtree_test.py
@@ -1,39 +1,51 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import os.path as P
+import subprocess
 
-if not os.path.exists("image2qtree"):
-    os.mkdir("image2qtree")
-os.chdir("image2qtree")
-os.system("../geotif-generate")
+TEST_DIR  = P.abspath(P.dirname(__file__))
+TOOLS_DIR = P.abspath(P.join(TEST_DIR, '..', 'vw', 'tools'))
+
+BUILD_DIR = P.join(TEST_DIR, 'image2qtree.out')
+QTREE     = P.join(TOOLS_DIR, 'image2qtree')
+
+if not os.path.exists(BUILD_DIR):
+    os.mkdir(BUILD_DIR)
+
+def run(*args, **kw):
+    print 'Running: %s' % ' '.join(args)
+    subprocess.check_call(args, cwd=kw.get('cwd', None))
+
+run(P.join(TEST_DIR, 'geotif-generate'), cwd=BUILD_DIR)
 
 # Create the image2qtree results
-files = os.listdir(".")
+files = os.listdir(BUILD_DIR)
 for i in files:
-    os.system("image2qtree -m kml %s" % i);
+    run(QTREE, '-m', 'kml', i, cwd=BUILD_DIR)
 
 # Create master kml
-mkml = open('master.kml','w')
+with file(P.join(BUILD_DIR, 'master.kml'), 'w') as mkml:
+    mkml.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+    mkml.write("<kml xmlns=\"http://earth.google.com/kml/2.2\">\n")
+    mkml.write("<Document>\n")
+    mkml.write("  <name>ListStyle radiofolder</name>\n")
+    mkml.write("  <Folder>\n")
+    mkml.write("    <name>Image2qtree results</name>\n")
+    mkml.write("    <Style>\n")
+    mkml.write("      <ListStyle>\n")
+    mkml.write("        <listItemType>radioFolder</listItemType>\n")
+    mkml.write("      </ListStyle>\n")
+    mkml.write("    </Style>\n")
 
-mkml.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-mkml.write("<kml xmlns=\"http://earth.google.com/kml/2.2\">\n")
-mkml.write("<Document>\n")
-mkml.write("  <name>ListStyle radiofolder</name>\n")
-mkml.write("  <Folder>\n")
-mkml.write("    <name>Image2qtree results</name>\n")
-mkml.write("    <Style>\n")
-mkml.write("      <ListStyle>\n")
-mkml.write("        <listItemType>radioFolder</listItemType>\n")
-mkml.write("      </ListStyle>\n")
-mkml.write("    </Style>\n")
+    for i in files:
+        i = P.basename(i)
+        prefix = i[:-4]
+        mkml.write("    <NetworkLink>\n")
+        mkml.write("      <name>%s</name><refreshVisibility>1</refreshVisibility>\n" % i)
+        mkml.write("      <Link><href>%s/%s.kml</href></Link>\n" % (prefix, prefix))
+        mkml.write("    </NetworkLink>\n")
 
-for i in files:
-    prefix = i[:-4]
-    mkml.write("    <NetworkLink>\n")
-    mkml.write("      <name>%s</name><refreshVisibility>1</refreshVisibility>\n" % i)
-    mkml.write("      <Link><href>%s/%s.kml</href></Link>\n" % (prefix, prefix))
-    mkml.write("    </NetworkLink>\n")
-
-mkml.write("  </Folder>\n")
-mkml.write("</Document>\n")
-mkml.write("</kml>\n")
+    mkml.write("  </Folder>\n")
+    mkml.write("</Document>\n")
+    mkml.write("</kml>\n")
