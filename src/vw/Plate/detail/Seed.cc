ec566231b6a6c5444d1bbe13a2168c1273e92243
Mike Lundy <mike@fluffypenguin.org>
2011-05-18 18:09:45 -0700

    rework plate random seed again

--- src/vw/Plate/detail/Seed.cc
+++ src/vw/Plate/detail/Seed.cc
@@ -7,14 +7,22 @@
 
 #include <vw/Plate/detail/Seed.h>
 #include <vw/Core/Thread.h>
+#include <vw/Core/Log.h>
+#include <vw/Core/Exception.h>
 #include <vw/Core/Stopwatch.h>
-#include <sys/types.h>
+#include <unistd.h>
 
 namespace {
-  vw::RunOnce seed_once = VW_RUNONCE_INIT;
+  using namespace vw;
+  RunOnce seed_once = VW_RUNONCE_INIT;
 
   void hidden_seed_random() {
-    srandom(vw::Stopwatch::microtime()*::getpid());
+    uint64 seed = Stopwatch::microtime();
+    unsigned int seed32 = (unsigned int)(seed & 0x00000000ffffffff);
+    unsigned int pid = (unsigned int)::getpid();
+    seed32 ^= pid;
+    vw_out(DebugMessage) << "Seeding RNG with " << seed32 << std::endl;
+    ::srandom(seed32);
   }
 }
 
