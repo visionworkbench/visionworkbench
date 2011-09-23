403e99e2883528e6dd7300882b4f45826e1e4839
Kyle Husmann <kyle.husmann@gmail.com>
2011-09-23 12:44:39 -0700

    added a protobuf description for Datum, as well as a quick test for it

---
+++ src/vw/Cartography/tests/TestDatum.cxx
@@ -0,0 +1,29 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2011 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+
+// TestDatum.h
+#include <gtest/gtest.h>
+#include <test/Helpers.h>
+
+#include <vw/Cartography/Datum.h>
+
+using namespace vw;
+using namespace vw::cartography;
+using namespace vw::test;
+
+
+#if defined(VW_HAVE_PKG_PROTOBUF) && VW_HAVE_PKG_PROTOBUF==1
+TEST( Datum, DatumDesc ) {
+  Datum datum("NAD27");
+
+  DatumDesc desc = datum.build_desc();
+
+  Datum datum2(desc);
+
+  EXPECT_EQ(datum.build_desc().DebugString(), datum2.build_desc().DebugString());
+}
+#endif
