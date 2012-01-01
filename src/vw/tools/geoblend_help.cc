e99f8c246094fc9b6ed34a7baa6c6ac34f85d123
Zack Moratto <zachary.m.moratto@nasa.gov>
2011-12-31 19:15:17 -0800

    geoblend: Split into multiple archives
    This is speed up perceived compile time and to reduce instantaneous
    memory usage. The thread to compile geoblend uses about 1.6G by itself
    in gcc4.2. Now that it has been split down, no single thread uses over
    700 MB.
    
    I'm using archives only because it is the only way in automake to
    compile the same source many times with different CPP
    flags. Geoblend_help.cc is the source for each archive, it just takes
    a macro to define which version it is instantiating.
    
    The downsides of this method is that more hard drive space is used (an
    additional 40MB) and the Makefile.am looks a little ugly.

---
+++ src/vw/tools/geoblend_help.cc
@@ -0,0 +1,27 @@
+// __BEGIN_LICENSE__
+// Copyright (C) 2006-2011 United States Government as represented by
+// the Administrator of the National Aeronautics and Space Administration.
+// All Rights Reserved.
+// __END_LICENSE__
+
+// The helper source file is used to instantian different pixel
+// versions of geoblend into different object files. It works by using
+// the PIXEL_TYPE macro.
+
+#include <vw/Tools/geoblend.h>
+
+using namespace vw;
+
+#define INSTANTIATE_CUSTOM_BLEND(PIXELTYPEMACRO, CHANNELTYPE) INSTANTIATE_CUSTOM_BLEND_(PIXELTYPEMACRO, CHANNELTYPE)
+
+#define INSTANTIATE_CUSTOM_BLEND_(PIXELTYPEMACRO, CHANNELTYPE) INSTANTIATE_CUSTOM_BLEND__(PIXELTYPEMACRO, CHANNELTYPE, PIXELTYPEMACRO ## _ ## CHANNELTYPE)
+
+#define INSTANTIATE_CUSTOM_BLEND__(PIXELTYPEMACRO, CHANNELTYPE, FUNCSUFFIX) \
+  void do_blend_##FUNCSUFFIX(void) {                                    \
+    do_blend<PIXELTYPEMACRO<CHANNELTYPE > >();                          \
+  }
+
+INSTANTIATE_CUSTOM_BLEND( PIXEL_TYPE, uint8 )
+INSTANTIATE_CUSTOM_BLEND( PIXEL_TYPE, int16 )
+INSTANTIATE_CUSTOM_BLEND( PIXEL_TYPE, uint16 )
+INSTANTIATE_CUSTOM_BLEND( PIXEL_TYPE, float32 )
