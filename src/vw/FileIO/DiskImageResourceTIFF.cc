7c0b71e9017e37b78aa20d58b5665cd622ec0e9a
Zack Moratto <zachary.m.moratto@nasa.gov>
2012-04-02 01:12:19 -0700

    FileIO: Fix compile error in DiskImageResourceTIFF.cc
    This happend in commit 8981f816. I'm not sure why the bug only showed
    up now.

--- src/vw/FileIO/DiskImageResourceTIFF.cc
+++ src/vw/FileIO/DiskImageResourceTIFF.cc
@@ -67,7 +67,7 @@ namespace vw {
 static void tiff_warning_handler(const char* module, const char* frmt, va_list ap) {
   char msg[VW_ERROR_BUFFER_SIZE];
   vsnprintf( msg, VW_ERROR_BUFFER_SIZE, frmt, ap );
-  vw::VW_OUT(vw::WarningMessage, "fileio") << "DiskImageResourceTIFF (" << (module?module:"none") << ") Warning: " << msg << std::endl;
+  VW_OUT(vw::WarningMessage, "fileio") << "DiskImageResourceTIFF (" << (module?module:"none") << ") Warning: " << msg << std::endl;
 }
 
 /// Handle libTIFF error conditions by vw_throwing an IOErr with the
