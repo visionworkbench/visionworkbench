646f69b84401b3808a4d908bfb88667a4cbc6eec
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2013-02-01 16:44:17 -0800

    CameraBBox: Unit test for intersecting ray with DEM

--- src/vw/Cartography/tests/TestCameraBBox.cxx
+++ src/vw/Cartography/tests/TestCameraBBox.cxx
@@ -34,12 +34,17 @@ using namespace vw::camera;
 class CameraBBoxTest :  public ::testing::Test {
 protected:
   virtual void SetUp() {
-    apollo = boost::shared_ptr<CameraModel>(new PinholeModel(TEST_SRCDIR"/apollo.pinhole"));
-    moon.set_well_known_geogcs("D_MOON");
+    apollo_camera = boost::shared_ptr<CameraModel>(new PinholeModel(TEST_SRCDIR"/apollo.pinhole"));
+    moon_georef.set_well_known_geogcs("D_MOON");
+    DEM.set_size(20,20); // DEM covering lat {10,-10} long {80,100}
+    for ( int32 i = 0; i < DEM.cols(); i++ )
+      for ( int32 j = 0; j <DEM.rows(); j++ )
+        DEM(i,j) = 1000 - 10*(pow(DEM.cols()/2.0-i,2.0)+pow(DEM.rows()/2.0-j,2.0));
   }
-
-  boost::shared_ptr<CameraModel> apollo;
-  GeoReference moon;
+  
+  boost::shared_ptr<CameraModel> apollo_camera;
+  GeoReference moon_georef;
+  ImageView<float> DEM;
 };
 
 TEST_F( CameraBBoxTest, GeospatialIntersectDatum ) {
@@ -47,43 +52,64 @@ TEST_F( CameraBBoxTest, GeospatialIntersectDatum ) {
     Vector2 input_image( rand()%4096, rand()%4096 );
     bool did_intersect;
     Vector2 lonlat =
-      geospatial_intersect( input_image, moon,
-                            apollo, did_intersect );
+      geospatial_intersect( input_image, moon_georef,
+                            apollo_camera, did_intersect );
     ASSERT_TRUE( did_intersect );
-    double radius = moon.datum().radius( lonlat[0], lonlat[1] );
+    double radius = moon_georef.datum().radius( lonlat[0], lonlat[1] );
     EXPECT_NEAR( radius, 1737400, 1e-3 );
     Vector3 llr( lonlat[0], lonlat[1], 0 );
-    Vector3 ecef = moon.datum().geodetic_to_cartesian(llr);
-    Vector3 llr2 = moon.datum().cartesian_to_geodetic(ecef);
+    Vector3 ecef = moon_georef.datum().geodetic_to_cartesian(llr);
+    Vector3 llr2 = moon_georef.datum().cartesian_to_geodetic(ecef);
     EXPECT_VECTOR_NEAR( llr2, llr, 1e-4 );
 
-    Vector2 retrn_image = apollo->point_to_pixel( ecef );
+    Vector2 retrn_image = apollo_camera->point_to_pixel( ecef );
     EXPECT_VECTOR_NEAR( retrn_image, input_image, 1e-3 );
   }
 }
 
 TEST_F( CameraBBoxTest, CameraBBoxDatum ) {
   float scale; // degrees per pixel
-  BBox2 image_bbox = camera_bbox( moon, apollo, 4096, 4096, scale );
+  BBox2 image_bbox = camera_bbox( moon_georef, apollo_camera, 4096, 4096, scale );
   EXPECT_VECTOR_NEAR( image_bbox.min(), Vector2(86,-1), 2 );
   EXPECT_VECTOR_NEAR( image_bbox.max(), Vector2(95,7), 2 );
   EXPECT_NEAR( scale, (95-86.)/sqrt(4096*4096*2), 1e-3 ); // Cam is rotated
 }
 
 TEST_F( CameraBBoxTest, CameraBBoxDEM ) {
-  ImageView<float> DEM(20,20); // DEM covering lat {10,-10} long {80,100}
-  for ( uint32 i = 0; i < 20; i++ )
-    for ( uint32 j = 0; j <20; j++ )
-      DEM(i,j) = 1000 - 10*(pow(10.-i,2.)+pow(10.-j,2));
   Matrix<double> geotrans = vw::math::identity_matrix<3>();
   geotrans(0,2) = 80;
   geotrans(1,1) = -1;
   geotrans(1,2) = 10;
-  moon.set_transform(geotrans);
+  moon_georef.set_transform(geotrans);
 
-  BBox2 image_bbox = camera_bbox( DEM, moon, apollo, 4096, 4096 );
+  BBox2 image_bbox = camera_bbox( DEM, moon_georef, apollo_camera, 4096, 4096 );
   EXPECT_VECTOR_NEAR( image_bbox.min(), Vector2(87,0), 2 );
   EXPECT_VECTOR_NEAR( image_bbox.max(), Vector2(94,6), 2 );
+}
+
+TEST_F( CameraBBoxTest, CameraPixelToXYZ) {
+  Matrix<double> geotrans = vw::math::identity_matrix<3>();
+  geotrans(0,2) = 80;
+  geotrans(1,1) = -1;
+  geotrans(1,2) = 10;
+  moon_georef.set_transform(geotrans);
+
+  
+  Vector2 input_pixel(50,10);
+
+  bool has_intersection;
+  Vector3 xyz = camera_pixel_to_dem_xyz(input_pixel,  
+                                        DEM,  
+                                        moon_georef,  
+                                        apollo_camera,  
+                                        has_intersection
+                                        );
+
+  // Verification
+  Vector2 output_pixel = apollo_camera->point_to_pixel(xyz);
+
+  EXPECT_EQ(has_intersection, 1);
+  EXPECT_VECTOR_NEAR(input_pixel, output_pixel, 1e-8);
 }
 
 #endif
