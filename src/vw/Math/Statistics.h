694d383503aaac852b601b7f39ece9a95309797b
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2014-03-17 11:54:48 -0700

    Statistics: Add function for outlier detection

--- src/vw/Math/Statistics.h
+++ src/vw/Math/Statistics.h
@@ -132,6 +132,41 @@ namespace math {
     }
   };
 
+
+  template<class T>
+  void find_outlier_brackets(std::vector<T> const& p, double outlier_factor,
+                             double & b, double & e){
+  
+    // Using the quartile range, determine the values b and e so that
+    // all elements in p outsize of [b, e] are outliers.
+
+    // If Q1 and Q3 are the lower and upper quartiles, keep points
+    // between Q1-outlier_factor*(Q3-Q1) and Q3 + outlier_factor*(Q3-Q1).
+
+    // This algorithm works best if the data is distributed rather
+    // uniformly.
+  
+    // Suggested value of outlier_factor: 1.5.
+  
+    b = 0.0; e = 0.0; // initialize
+    std::vector<T> q = p;
+    std::sort(q.begin(), q.end());
+    int len = q.size();
+    if (len <= 0) return;
+    b = q[0]; e = q[len-1];
+    if (len <= 3) return; // too few points for analysis
+      
+    int bn = int(round(0.25*len));
+    int en = int(round(0.75*len))-1;
+      
+    double Q1 = q[bn];
+    double Q3 = q[en];
+    b = Q1 - outlier_factor*(Q3-Q1);
+    e = Q3 + outlier_factor*(Q3-Q1);
+
+    return;
+  }
+  
 }} // namespace vw::math
 
 #endif // __MATH_STATISTICS_H__
