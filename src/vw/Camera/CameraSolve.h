efc79cc64f20c67bc39995ed00667f2309a91167
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2016-04-07 10:03:50 -0700

    Add CameraSolve.h

---
+++ src/vw/Camera/CameraSolve.h
@@ -0,0 +1,67 @@
+// __BEGIN_LICENSE__
+//  Copyright (c) 2006-2013, United States Government as represented by the
+//  Administrator of the National Aeronautics and Space Administration. All
+//  rights reserved.
+//
+//  The NASA Vision Workbench is licensed under the Apache License,
+//  Version 2.0 (the "License"); you may not use this file except in
+//  compliance with the License. You may obtain a copy of the License at
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+// __END_LICENSE__
+
+
+/// \file CameraSolve.h
+///
+/// Contains assorted functions for solving for camera matrices.
+/// This is an expansion on Math/Geometry.h but requires that
+/// container p1 is Vector3 and container p2 is Vector4 since
+/// cameras map 3D points to 2D locations.
+///
+#ifndef __CAMERA_CAMERA_SOLVE_H__
+#define __CAMERA_CAMERA_SOLVE_H__
+
+#include <vw/Camera/CameraModel.h>
+#include <vw/Math/LevenbergMarquardt.h>
+
+namespace vw {
+
+namespace camera {
+
+  // Levenberg Marquardt solver for line number (y) and column
+  // number (x) for the given point in space. The obtained solution
+  // pixel (x, y) must be such that the vector from this camera
+  // pixel goes through the given point. This version does uses only
+  // functions declared in CameraModel class and should work for 
+  // any camera. Some cameras may be able to use a faster
+  // implementation that solves for y first and then for x separately.
+  class CameraGenericLMA : public math::LeastSquaresModelBase<CameraGenericLMA> {
+    const CameraModel* m_model;
+    Vector3 m_point;
+  public:
+    typedef Vector2 domain_type;     // 2D pixel, input to cost function vector
+    typedef Vector3 result_type;     // 3D error, output of cost function vector
+    typedef Matrix<double, 3, 2> jacobian_type;
+
+    CameraGenericLMA( const CameraModel* model, const vw::Vector3& pt ) :
+      m_model(model), m_point(pt) {}
+
+    // Minimize the difference between the vector from the pixel and
+    //  the vector from the point to the camera.
+    inline result_type operator()( domain_type const& pix ) const {
+      try {
+        return m_model->pixel_to_vector(pix) - normalize(m_point - m_model->camera_center(pix));
+      } catch(...) { // Handle camera misses by returning a very large error
+        return Vector3(999999,999999,999999);
+      }
+    }
+  }; // End class CameraGenericLMA
+  
+}} // end vw::camera
+
+#endif//__CAMERA_CAMERA_SOLVE_H__
