d5bb23f8146ceee0a05ba7c1472c95b7a8f5fcc4
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2017-01-17 13:35:13 -0800

    make undistort_image work on large images, also save undistorted camera model

--- src/vw/tools/undistort_image.cc
+++ src/vw/tools/undistort_image.cc
@@ -33,11 +33,15 @@
 #include <vw/Camera/LensDistortion.h>
 #include <vw/FileIO/FileUtils.h>
 #include <vw/tools/Common.h>
+#include <vw/Cartography/GeoReferenceUtils.h>
 
 #include <iostream>
 
 #include <boost/program_options.hpp>
+#include <boost/filesystem/path.hpp>
+
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
 
 using namespace vw;
 using vw::camera::PinholeModel;
@@ -47,6 +51,79 @@ using vw::camera::LensDistortion;
 // with many channels and channel types.
 std::string input_file_name, output_file_name, camera_file_name;
 
+
+template <class ImageT>
+class UndistortView: public ImageViewBase< UndistortView<ImageT> >{
+  ImageT m_dist_img;
+  int m_cols, m_rows;
+  Vector2 m_offset;
+  PinholeModel m_camera_model;
+
+  typedef typename ImageT::pixel_type PixelT;
+
+public:
+  UndistortView(ImageT const& dist_img, int cols, int rows,
+                Vector2 const& offset, 
+                PinholeModel const& camera_model):
+    m_dist_img(dist_img), m_cols(cols), m_rows(rows),
+    m_offset(offset), m_camera_model(camera_model){}
+  
+  typedef PixelT pixel_type;
+  typedef PixelT result_type;
+  typedef ProceduralPixelAccessor<UndistortView> pixel_accessor;
+
+  inline int32 cols() const { return m_cols; }
+  inline int32 rows() const { return m_rows; }
+  inline int32 planes() const { return 1; }
+
+  inline pixel_accessor origin() const { return pixel_accessor( *this, 0, 0 ); }
+
+  inline pixel_type operator()( double/*i*/, double/*j*/, int32/*p*/ = 0 ) const {
+    vw_throw(NoImplErr() << "UndistortView::operator()(...) is not implemented");
+    return pixel_type();
+  }
+
+  typedef CropView<ImageView<pixel_type> > prerasterize_type;
+  inline prerasterize_type prerasterize(BBox2i const& bbox) const {
+
+    InterpolationView<EdgeExtensionView<ImageT, ZeroEdgeExtension>, BilinearInterpolation>
+      interp_dist_img = interpolate(m_dist_img, BilinearInterpolation(), ZeroEdgeExtension());
+
+    const LensDistortion* lens_ptr = m_camera_model.lens_distortion();
+    const double pitch = m_camera_model.pixel_pitch();
+
+    ImageView<result_type> tile(bbox.width(), bbox.height());
+
+    for (int col = bbox.min().x(); col < bbox.max().x(); col++){
+      for (int row = bbox.min().y(); row < bbox.max().y(); row++){
+
+        Vector2 lens_loc = elem_prod(Vector2(col, row) + m_offset, pitch);
+        Vector2 out_loc  = lens_ptr->distorted_coordinates(m_camera_model, lens_loc);
+        Vector2 in_loc = elem_quot(out_loc, pitch);
+
+        tile(col - bbox.min().x(), row - bbox.min().y() )
+          = interp_dist_img(in_loc[0], in_loc[1]);
+      }
+    }
+    
+    return prerasterize_type(tile, -bbox.min().x(), -bbox.min().y(),
+                             cols(), rows() );
+  }
+
+  template <class DestT>
+  inline void rasterize(DestT const& dest, BBox2i bbox) const {
+    vw::rasterize(prerasterize(bbox), dest, bbox);
+  }
+};
+template <class ImageT>
+UndistortView<ImageT> undistort_image(ImageT const& dist_img,
+                                      int cols, int rows,
+                                      Vector2 const& offset, 
+                                      PinholeModel const& camera_model){
+  return UndistortView<ImageT>(dist_img, cols, rows, offset, camera_model);
+}
+
+
 /// Generates an undistorted view of an input image.
 /// - This should be moved somewhere else.
 /// - Could easily add the option to keep the image the same size.
@@ -54,28 +131,18 @@ template <class PixelT>
 void do_work() {
 
   // Load the input image
-  printf("Loading input image: %s\n", input_file_name.c_str());
-  ImageView<PixelT> image_in;
-  read_image(image_in, input_file_name);
-
-  // Interpolate the input image
-  InterpolationView<EdgeExtensionView<ImageView<PixelT>, ZeroEdgeExtension>, BilinearInterpolation> interp_image_in = interpolate(image_in, BilinearInterpolation(), ZeroEdgeExtension());
-  
-  // Output image
-  ImageView<PixelT> image_out;
+  vw_out() << "Loading input image: " << input_file_name << "\n";
+  ImageView<PixelT> dist_img;
+  read_image(dist_img, input_file_name);
 
   // Load the camera
-  printf("Loading camera model file: %s\n", camera_file_name.c_str());
+  vw_out() << "Loading camera model file: " << camera_file_name << "\n";
   PinholeModel camera_model(camera_file_name);
   
-  printf("Undistorting the image...\n");
-
-  const int width_in  = interp_image_in.cols();
-  const int height_in = interp_image_in.rows();
+  const int width_in  = dist_img.cols();
+  const int height_in = dist_img.rows();
   const LensDistortion* lens_ptr = camera_model.lens_distortion();
   const double pitch = camera_model.pixel_pitch();
-
-  //std::cout << camera_model << std::endl;
 
   // Figure out the size of the undistorted image
   // - Iterate along each side of the image and record where the output pixels go
@@ -98,36 +165,41 @@ void do_work() {
     lens_loc = elem_prod(Vector2(c, height_in-1), pitch);
     out_loc  = lens_ptr->undistorted_coordinates(camera_model, lens_loc);
     output_area.grow(elem_quot(out_loc, pitch));
-    //std::cout << lens_loc << " --> " << out_loc << " --> " << elem_quot(out_loc, pitch) << std::endl;
   }
-  //std::cout << camera_model << std::endl;
-  image_out.set_size(floor(output_area.width()), floor(output_area.height()));
+
   Vector2 offset = output_area.min();
 
-  vw::TerminalProgressCallback tpc("asp", "\t--> ");
-  double inc_amount = 1.0 / double(image_out.rows());
-  tpc.report_progress(0);
-  
-  // Fill in the undistorted image
-  for (int r=0; r<image_out.rows(); ++r) {
-    for (int c=0; c<image_out.cols(); ++c) {
-      // For each output pixel, find the associated input pixel.
-      // - Need to add in an offset so that the pixel coords we pass in are in the coordinate
-      //   frame of the input image rather than of the output image.
-      lens_loc = elem_prod(Vector2(c,r)+offset, pitch);
-      //std::cout << lens_loc << std::endl;
-      out_loc  = lens_ptr->distorted_coordinates(camera_model, lens_loc);
-      Vector2 in_loc = elem_quot(out_loc, pitch);
-      image_out(c,r) = interp_image_in(in_loc[0], in_loc[1]);
-    }
-    tpc.report_incremental_progress( inc_amount );
-  } // End double loop through output image
-  tpc.report_finished();
+  int cols = floor(output_area.width());
+  int rows = floor(output_area.height());
+  vw_out() << "Output image size: " << cols << ' ' << rows << std::endl;
 
-  printf("Writing output image: %s\n", output_file_name.c_str());
-  write_image(output_file_name, image_out);
+  vw::cartography::GdalWriteOptions write_options;
+
+  bool has_georef = false;
+  cartography::GeoReference georef;
+  double nodata = 0;
+  bool has_nodata = false;  // TODO: May need to set this to true, at least for grayscale images
+  vw_out() << "Writing: " << output_file_name << std::endl;
+  TerminalProgressCallback tpc("vw", "");
+  block_write_gdal_image(output_file_name,
+
+                         undistort_image(dist_img,  
+                                         cols, rows,  
+                                         offset,  
+                                         camera_model),
+                         has_georef,  
+                         georef, has_nodata, nodata,  
+                         write_options, tpc);
+
+  // Save the camera model for the undistorted image
+  PinholeModel out_model = vw::camera::strip_lens_distortion(camera_model);
+  out_model.set_point_offset(out_model.point_offset() - offset*out_model.pixel_pitch());
   
-  printf("Finished!\n");
+  std::string out_cam_path = fs::path(output_file_name).replace_extension(".tsai").string();
+  vw_out() << "Writing: " << out_cam_path << std::endl;
+  out_model.write(out_cam_path);
+  
+  vw_out() << "Finished!\n";
   
 } // End function do_work
 
@@ -181,6 +253,8 @@ void do_work_all_channels(std::string const& input_file_name){
     
 int main( int argc, char *argv[] ) {
 
+  // TODO: Output better be float?
+  
   po::options_description desc("Usage: undistort_image [options] <input image> <camera model> \n\nOptions");
   desc.add_options()
     ("help,h",        "Display this help message")
@@ -199,19 +273,19 @@ int main( int argc, char *argv[] ) {
     po::store( po::command_line_parser( argc, argv ).options(desc).positional(p).run(), vm );
     po::notify( vm );
   } catch (const po::error& e) {
-    std::cout << "An error occured while parsing command line arguments.\n";
-    std::cout << "\t" << e.what() << "\n\n";
-    std::cout << desc;
+    vw_out() << "An error occured while parsing command line arguments.\n";
+    vw_out() << "\t" << e.what() << "\n\n";
+    vw_out() << desc;
     return 1;
   }
 
   if( vm.count("help") ) {
-    std::cout << desc << std::endl;
+    vw_out() << desc << std::endl;
     return 1;
   }
   if( (vm.count("input-file") != 1) || (vm.count("camera-file") != 1) ) {
-    std::cout << "Error: Must specify exactly one image file and one camera file!" << std::endl;
-    std::cout << desc << std::endl;
+    vw_out() << "Error: Must specify exactly one image file and one camera file!" << std::endl;
+    vw_out() << desc << std::endl;
     return 1;
   }
 
