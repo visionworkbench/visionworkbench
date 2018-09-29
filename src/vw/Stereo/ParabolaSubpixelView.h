10088e4e2a90cb60d30824dd595c2ca8ebd88c5a
Scott <scott.t.mcmichael@nasa.gov>
2018-09-29 11:13:44 -0700

    Fix typo

--- src/vw/Stereo/ParabolaSubpixelView.h
+++ src/vw/Stereo/ParabolaSubpixelView.h
@@ -9,8 +9,7 @@
 //  http://www.apache.org/licenses/LICENSE-2.0
 //
 //  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS
-" BASIS,
+//  distributed under the License is distributed on an "AS IS" BASIS,
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
@@ -422,4 +421,4 @@ namespace stereo {
 
 }} // namespace vw::stereo
 
-#endif // __VW_STEREO_CORRELATOR_VIEW__
+#endif // __VW_STEREO_PARABOLASUBPIXEL_VIEW__
