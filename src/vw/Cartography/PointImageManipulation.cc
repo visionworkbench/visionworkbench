47d60636d7218feb68a1a8bd3d2aeb8be801693f
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2024-10-21 12:29:11 -0700

    Bugfix for failing to project

--- src/vw/Cartography/PointImageManipulation.cc
+++ src/vw/Cartography/PointImageManipulation.cc
@@ -21,38 +21,46 @@
 
 using namespace vw;
 
-Vector3 cartography::GeodeticToCartesian::operator()( Vector3 const& v ) const {
-  if ( boost::math::isnan(v[2]) )
+Vector3 cartography::GeodeticToCartesian::operator()(Vector3 const& v) const {
+  if (boost::math::isnan(v[2]))
     return Vector3();
   return m_datum.geodetic_to_cartesian(v);
 }
 
-Vector3 cartography::CartesianToGeodetic::operator()( Vector3 const& v ) const {
-  if ( v == Vector3() )
-    return Vector3(0,0,std::numeric_limits<double>::quiet_NaN());
+Vector3 cartography::CartesianToGeodetic::operator()(Vector3 const& v) const {
+  if (v == Vector3())
+    return Vector3(0, 0, std::numeric_limits<double>::quiet_NaN());
   return m_datum.cartesian_to_geodetic(v);
 }
 
-Vector3 cartography::GeodeticToProjection::operator()( Vector3 const& v ) const {
-  if ( boost::math::isnan(v[2]) )
+Vector3 cartography::GeodeticToProjection::operator()(Vector3 const& v) const {
+  if (boost::math::isnan(v[2]))
     return v;
-  Vector2 pix = m_reference.lonlat_to_pixel( subvector(v, 0, 2) );
-  return Vector3( pix[0], pix[1], v[2] );
+  Vector2 pix = m_reference.lonlat_to_pixel(subvector(v, 0, 2));
+  return Vector3(pix[0], pix[1], v[2]);
 }
 
-Vector3 cartography::ProjectionToGeodetic::operator()( Vector3 const& v ) const {
-  Vector2 ll = m_reference.pixel_to_lonlat( subvector( v, 0, 2 ) );
-  return Vector3( ll[0], ll[1], v[2] );
+Vector3 cartography::ProjectionToGeodetic::operator()(Vector3 const& v) const {
+  Vector2 ll = m_reference.pixel_to_lonlat(subvector(v, 0, 2));
+  return Vector3(ll[0], ll[1], v[2]);
 }
 
-Vector3 cartography::GeodeticToPoint::operator()( Vector3 const& v ) const {
-  if ( boost::math::isnan(v[2]) )
+Vector3 cartography::GeodeticToPoint::operator()(Vector3 const& v) const {
+  double nan = std::numeric_limits<double>::quiet_NaN();
+  if (boost::math::isnan(v[2]))
     return v;
-  Vector2 pix = m_reference.lonlat_to_point( subvector(v, 0, 2) );
-  return Vector3( pix[0], pix[1], v[2] );
+  Vector2 pix;
+  try {
+    pix = m_reference.lonlat_to_point(subvector(v, 0, 2));
+  } catch (...) {
+    // This is a bugfix. Catch failures from GDAL.
+    return Vector3(0, 0, nan);
+  }
+  
+  return Vector3(pix[0], pix[1], v[2]);
 }
 
-Vector3 cartography::PointToGeodetic::operator()( Vector3 const& v ) const {
-  Vector2 ll = m_reference.point_to_lonlat( subvector( v, 0, 2 ) );
-  return Vector3( ll[0], ll[1], v[2] );
+Vector3 cartography::PointToGeodetic::operator()(Vector3 const& v) const {
+  Vector2 ll = m_reference.point_to_lonlat(subvector(v, 0, 2));
+  return Vector3(ll[0], ll[1], v[2]);
 }
